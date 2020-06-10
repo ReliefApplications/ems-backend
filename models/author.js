a5b4152e8d7df9bbc9e3da19b08503ff35d43f59
antoineHurard <antoine.reliefapps@gmail.com>
2020-06-10 15:11:29 +0200

    Change package name

--- models/author.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const authorSchema = new Schema({
-    name: String,
-    age: Number
-});
-
-module.exports = mongoose.model('Author', authorSchema);
