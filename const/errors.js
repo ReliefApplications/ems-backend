2735ee7c3f4cdd8a984ef2074e6e0429218f00d4
antoineHurard <antoine.reliefapps@gmail.com>
2020-07-28 19:52:04 +0200

    Errors const update

--- const/errors.js
+++ const/errors.js
@@ -1,6 +1,11 @@
 const errors = {
     userNotLogged: 'You must be connected.',
     permissionNotGranted: 'Permission not granted.',
+    invalidAddFormArguments: 'Form should either correspond to a new resource or existing resource.',
+    invalidEditResourceArguments: 'Either fields or permissions must be provided.',
+    invalidAddDashboardArguments: 'Name must be provided.',
+    invalidEditDashboardArguments: 'Either name, structure or permissions must be provided.',
+    resourceDuplicated: 'An existing resource with that name already exists.',
     // TODO fix
     missingDataField: 'Please add a value name to all questions, inside Data tab.',
     dataFieldDuplicated: (name) => `Data name duplicated : ${name}. Please provide different value names for all questions.`
