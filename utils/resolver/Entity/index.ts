7e3d99f9b34aadd3ffeb64fec22478a233b705c4
Antoine Hurard <antoine.reliefapps@gmail.com>
2020-12-09 09:07:26 +0100

    Classic resolvers for entities fields are set

--- utils/resolver/Entity/index.ts
+++ utils/resolver/Entity/index.ts
@@ -1,44 +1,56 @@
 import getFields from "../../introspection/getFields";
 import { getRelatedType, getRelatedKey, getReverseRelatedField, getRelationshipFromKey } from "../../introspection/getTypeFromKey";
 import { isRelationshipField } from "../../introspection/isRelationshipField";
+import { isNotRelationshipField } from "../../introspection/isNotRelationshipField";
 
 export default (entityName, data) => {
     const entityFields = Object.keys(getFields(data[entityName]));
-    const manyToOneResolvers = entityFields.filter(isRelationshipField).reduce(
+
+    // const manyToOneResolvers = entityFields.filter(isRelationshipField).reduce(
+    //     (resolvers, fieldName) =>
+    //         Object.assign({}, resolvers, {
+    //             [getRelatedType(fieldName)]: (entity) => {
+    //                 console.log('field');
+    //                 console.log(entity);
+    //                 return data[getRelatedKey(fieldName)].find(
+    //                     (relatedRecord) => relatedRecord.id == entity[fieldName]
+    //                 );
+    //             }
+    //         }),
+    //     {}
+    // );
+
+    const classicResolvers = entityFields.filter(isNotRelationshipField).filter(x => x !== 'id').reduce(
         (resolvers, fieldName) =>
             Object.assign({}, resolvers, {
-                [getRelatedType(fieldName)]: (entity) => {
-                    console.log('field');
-                    console.log(fieldName);
-                    return data[getRelatedKey(fieldName)].find(
-                        (relatedRecord) => relatedRecord.id == entity[fieldName]
-                    );
+                [fieldName]: (entity) => {
+                    return entity.data[fieldName];
                 }
             }),
         {}
     );
 
-    const relatedField = getReverseRelatedField(entityName); // 'posts' => 'post_id'
+    // const relatedField = getReverseRelatedField(entityName); // 'posts' => 'post_id'
 
-    const hasReverseRelationship = (entityName) =>
-        Object.keys(getFields(data[entityName])).includes(
-            relatedField
-        );
+    // const hasReverseRelationship = (entityName) =>
+    //     Object.keys(getFields(data[entityName])).includes(
+    //         relatedField
+    //     );
 
-    const entities = Object.keys(data);
-    const oneToManyResolvers = entities.filter(hasReverseRelationship).reduce(
-        (resolvers, entityName) =>
-            Object.assign({}, resolvers, {
-                [getRelationshipFromKey(entityName)]: (entity) => {
-                    console.log('related');
-                    console.log(relatedField);
-                    return data[entityName].filter(
-                        (record) => record[relatedField] == entity.id
-                    );
-                }
-            }),
-        {}
-    );
+    // const entities = Object.keys(data);
+    // const oneToManyResolvers = entities.filter(hasReverseRelationship).reduce(
+    //     (resolvers, entityName) =>
+    //         Object.assign({}, resolvers, {
+    //             [getRelationshipFromKey(entityName)]: (entity) => {
+    //                 console.log('related');
+    //                 console.log(relatedField);
+    //                 return data[entityName].filter(
+    //                     (record) => record[relatedField] == entity.id
+    //                 );
+    //             }
+    //         }),
+    //     {}
+    // );
 
-    return Object.assign({}, manyToOneResolvers, oneToManyResolvers);
+    return Object.assign({}, classicResolvers);
 };
