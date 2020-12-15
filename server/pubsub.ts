9edf7363361561c57da78bcaf7abcb6ad71de176
PacomeRivier <pacome@reliefapplications.org>
2020-12-15 09:08:48 +0100

    Add RabbitMQ link to pubsub

--- server/pubsub.ts
+++ server/pubsub.ts
@@ -1,5 +1,36 @@
-import { PubSub } from 'graphql-subscriptions';
+// https://github.com/Surnet/graphql-amqp-subscriptions
+import { AMQPPubSub } from 'graphql-amqp-subscriptions';
+import amqp from 'amqplib';
 
-const pubsub = new PubSub();
+let pubsub;
+
+amqp.connect('amqp://admin:admin@localhost:5672?heartbeat=30')
+.then(conn => {
+  pubsub = new AMQPPubSub({
+    connection: conn
+    /* exchange: {
+       name: 'exchange',
+       type: 'topic',
+       options: {
+         durable: false,
+         autoDelete: true
+       }
+     },
+     queue: {
+       name: 'queue'
+       options: {
+         exclusive: true,
+         durable: true,
+         autoDelete: true
+       },
+       unbindOnDispose: false;
+       deleteOnDispose: false;
+     } */
+  });
+  // Use the pubsub instance from here on
+})
+.catch(err => {
+  console.error(err);
+});
 
 export default pubsub;
