8c546782b7873d6d69451a8782ee82fac941b15e
Adi Subramanian <adi@oortcloud.tech>
2020-12-16 11:50:13 +0100

    Recursive delete Forms and records

--- schema/mutation/deleteResource.ts
+++ schema/mutation/deleteResource.ts
@@ -1,9 +1,9 @@
-import { GraphQLNonNull, GraphQLID } from "graphql";
+import { GraphQLNonNull, GraphQLID, GraphQLError } from "graphql";
 import permissions from "../../const/permissions";
 import checkPermission from "../../utils/checkPermission";
 import { ResourceType } from "../types";
 import mongoose from 'mongoose';
-import { Resource } from "../../models";
+import { Resource, Record, Form } from "../../models";
 
 export default {
     /*  Deletes a resource from its id.
@@ -16,6 +16,25 @@ export default {
     resolve(parent, args, context) {
         const user = context.user;
         if (checkPermission(user, permissions.canManageResources)) {
+            Record.find({
+                resource: args.id
+            })
+            .then(
+                res => res.map(record => Record.findByIdAndDelete(record._id))
+            )
+            .catch((err) => {
+                throw new GraphQLError(err)
+            })
+            
+            Form.find({
+                resource: args.id
+            })
+            .then(
+                res => res.map(form => Form.findByIdAndDelete(form._id))
+            )
+            .catch(err => {
+                throw new GraphQLError(err)
+            });
             return Resource.findByIdAndRemove(args.id);
         } else {
             const filters = {
