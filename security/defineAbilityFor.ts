d9722ff4fcc1408fa2ca5f09fa9e6103d488c9f0
Pacome Rivier <pacome@reliefapplications.org>
2021-02-16 14:49:52 +0100

    [SAFE-72] Re-define abilities only on new user

--- security/defineAbilityFor.ts
+++ security/defineAbilityFor.ts
@@ -10,7 +10,7 @@ type Actions = 'create' | 'read' | 'update' | 'delete';
 type Models = Application | Channel | 'Channel' | Dashboard | Form | Notification | Page | Permission | Record | Resource | Role | Step | User | Version | Workflow 
 type Subjects = InferSubjects<Models>;
 
-type AppAbility = Ability<[Actions, Subjects]>;
+export type AppAbility = Ability<[Actions, Subjects]>;
 const AppAbility = Ability as AbilityClass<AppAbility>;
 
 /*  Define a const for common filters on permissions
@@ -32,70 +32,97 @@ function filters(type: string, user: User) {
   }
 }
 
-/*  Define abilities for the given user.
+/*  Store the user and abilities so we don't redo it for each request
+ */
+let _user: User = null;
+let _ability: AppAbility = null;
+
+/*  Define abilities for the given user. Then store them and define them again only on user change.
  *  Define document users can create, read, update and delete.
  */
 export default function defineAbilitiesFor(user: User): AppAbility {
-  const { can, cannot, rules } = new AbilityBuilder(AppAbility);
-
-  // Default abilities
-  can(['read', 'create', 'update', 'delete'], 'Record');
-
-  // Permissions check abilities
-  if (checkPermission(user, permissions.canSeeApplications)) {
-    can('read', 'Application');
+  if (JSON.stringify(user) === JSON.stringify(_user)) {
+    return _ability;
   } else {
-    can('read', 'Application', filters('canSee', user));
-  }
-
-  if(checkPermission(user, permissions.canManageApplications)) {
-    can(['read', 'create', 'update', 'delete'], ['Application', 'Dashboard', 'Page', 'Step', 'Workflow', 'Channel']);
-  } else {
-    can('read', ['Application', 'Page', 'Step'], filters('canSee', user));
-    can('update', ['Application', 'Page', 'Step'], filters('canUpdate', user));
-    can('delete', ['Application', 'Page', 'Step'], filters('canDelete', user));
-  }
-
-  if (checkPermission(user, permissions.canSeeForms)) {
-    can('read', 'Form');
-  } else {
-    can('read', 'Form', filters('canSee', user));
-  }
-
-  if (checkPermission(user, permissions.canManageForms)) {
-    can(['create', 'update', 'delete'], 'Form');
-  } else {
-    can('update', 'Form', filters('canUpdate', user));
-    can('delete', 'Form', filters('canDelete', user));
-  }
-
-  if (checkPermission(user, permissions.canSeeRoles)) {
-    can(['create', 'read', 'update', 'delete'], 'Role');
-  } else {
-    let applications = [];
-    user.roles.map(role => {
-      if (role.application) {
-        if (role.permissions.some(perm => perm.type === permissions.canSeeRoles)) {
-          applications.push(mongoose.Types.ObjectId(role.application));
+    console.log('🛡️  Define abilities');
+    const { can, cannot, rules } = new AbilityBuilder(AppAbility);
+  
+    // Default abilities
+    can(['read', 'create', 'update', 'delete'], 'Record');
+  
+    // Permissions check abilities
+    if (checkPermission(user, permissions.canSeeApplications)) {
+      can('read', 'Application');
+    } else {
+      can('read', 'Application', filters('canSee', user));
+    }
+  
+    if(checkPermission(user, permissions.canManageApplications)) {
+      can(['read', 'create', 'update', 'delete'], ['Application', 'Dashboard', 'Page', 'Step', 'Workflow', 'Channel']);
+    } else {
+      can('read', ['Application', 'Page', 'Step'], filters('canSee', user));
+      can('update', ['Application', 'Page', 'Step'], filters('canUpdate', user));
+      can('delete', ['Application', 'Page', 'Step'], filters('canDelete', user));
+      const ability: AppAbility = new Ability(rules)
+      const contentFilters = Page.accessibleBy(ability, 'read');
+    }
+  
+    if (checkPermission(user, permissions.canSeeForms)) {
+      can('read', 'Form');
+    } else {
+      can('read', 'Form', filters('canSee', user));
+    }
+  
+    if (checkPermission(user, permissions.canManageForms)) {
+      can(['create', 'update', 'delete'], 'Form');
+    } else {
+      can('update', 'Form', filters('canUpdate', user));
+      can('delete', 'Form', filters('canDelete', user));
+    }
+  
+    if(checkPermission(user, permissions.canSeeResources)) {
+      can('read', 'Resource');
+    } else {
+      can('read', 'Resource', filters('canSee', user));
+    }
+  
+    if (checkPermission(user, permissions.canManageResources)) {
+      can(['create', 'update', 'delete'], 'Resource');
+    } else {
+      can('update', 'Resource', filters('canUpdate', user));
+      can('delete', 'Resource', filters('canDelete', user));
+    }
+  
+    if (checkPermission(user, permissions.canSeeRoles)) {
+      can(['create', 'read', 'update', 'delete'], 'Role');
+    } else {
+      let applications = [];
+      user.roles.map(role => {
+        if (role.application) {
+          if (role.permissions.some(perm => perm.type === permissions.canSeeRoles)) {
+            applications.push(mongoose.Types.ObjectId(role.application));
+          }
         }
-      }
-    });
-    can(['create', 'read', 'update', 'delete'], 'Role', {application: applications});
-  }
-
-  if (checkPermission(user, permissions.canSeeUsers)) {
-    can(['create', 'read', 'update', 'delete'], 'User');
-  } else {
-    let applications = [];
-    user.roles.map(role => {
-      if (role.application) {
-        if (role.permissions.some(perm => perm.type === permissions.canSeeUsers)) {
-          applications.push(mongoose.Types.ObjectId(role.application));
+      });
+      can(['create', 'read', 'update', 'delete'], 'Role', {application: applications});
+    }
+  
+    if (checkPermission(user, permissions.canSeeUsers)) {
+      can(['create', 'read', 'update', 'delete'], 'User');
+    } else {
+      let applications = [];
+      user.roles.map(role => {
+        if (role.application) {
+          if (role.permissions.some(perm => perm.type === permissions.canSeeUsers)) {
+            applications.push(mongoose.Types.ObjectId(role.application));
+          }
         }
-      }
-    });
-    can(['create', 'read', 'update', 'delete'], 'Role', {application: applications});
+      });
+      can(['create', 'read', 'update', 'delete'], 'Role', {application: applications});
+    }
+  
+    _user = user;
+    _ability = new Ability(rules);
+    return _ability;
   }
-
-  return new Ability(rules);
 };
