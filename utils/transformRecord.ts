7bc34020c291f1206ec936e01860fb13023dba1d
hugo <hugo.reliefapps@gmail.com>
2021-02-19 16:23:34 +0100

    [SAFE-9] modified if statement for time value

--- utils/transformRecord.ts
+++ utils/transformRecord.ts
@@ -1,5 +1,6 @@
 function transformRecord(data, fields) {
     for (const value in data) {
+        console.log(data);
         if (data.hasOwnProperty(value)) {
             const field = fields.find(x => x.name === value);
             switch (field.type) {
@@ -13,7 +14,7 @@ function transformRecord(data, fields) {
                     data[value] = new Date(data[value]);
                     break;
                 case 'time':
-                    if (!(data[value] instanceof Date)) {
+                    if (!(data[value] instanceof Date) && data[value] != null) {
                         const hours = data[value].slice(0, 2);
                         const minutes = data[value].slice(3);
                         data[value] = new Date(Date.UTC(1970, 0, 1, hours, minutes));
