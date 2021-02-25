a1f8d11eb8edd485e4f0434477fde6a5e3499990
Pacome Rivier <pacome@reliefapplications.org>
2021-02-25 12:32:17 +0100

    [SAFE-132] Set up the permission filter in a new way

--- utils/resolver/Query/all.ts
+++ utils/resolver/Query/all.ts
@@ -6,14 +6,7 @@ import { Form, Record } from "../../../models";
 import convertFilter from "../../convertFilter";
 import getFilter from "./getFilter";
 import getSortField from "./getSortField";
-import { AbilityBuilder, Ability, InferSubjects, AbilityClass } from '@casl/ability';
 import mongoose from 'mongoose';
-
-export type AppAbility = Ability<[Actions, Subjects]>;
-export type Actions = 'create' | 'read' | 'update' | 'delete';
-type Models = Record;
-export type Subjects = InferSubjects<Models>;
-const AppAbility = Ability as AbilityClass<AppAbility>;
 
 export default (id) => async (
     _,
@@ -25,15 +18,12 @@ export default (id) => async (
     if (!user) {
         throw new GraphQLError(errors.userNotLogged);
     }
-    const ability: AppAbility = user.ability;
 
     const mongooseFilter = getFilter(filter);
 
     Object.assign(mongooseFilter,
         { $or: [{ resource: id }, { form: id }] }
     );
-
-
 
     // if (!ability.can('read', 'Record')) {
     //     form.permissions.canQuery.forEach(x => {
@@ -47,29 +37,33 @@ export default (id) => async (
     access: everything -> can acess all records where id is defined by the top
 
     role: coordinator
-    access: everuthing part of same country ( based on creator )
+    access: everything part of same country ( based on creator )
 
     role: partner
     access: everything part of same country + same agency ( based on creator )
 
     */
-    const form = await Form.findById(id);
-    const { can, cannot, rules } = new AbilityBuilder(AppAbility);
-    // can('read', 'Record',)
+    const form = await Form.findOne({ $or: [{ resource: id }, { form: id }] });
 
     const roles = user.roles.map(x => mongoose.Types.ObjectId(x._id));
 
+    const permissionFilters = [];
+    //let permissionFilter = { 'createdBy.positionAttributes': { $elemMatch: { value: "France", category: mongoose.Types.ObjectId("6034e020bcffc3001f7003b0") } } };
+    
     form.permissions.canQuery.forEach(x => {
-        can('read', 'Record', { createdBy: user });
-        //         Object.assign(mongooseFilter,
-        //             convertFilter(x.access, Record, user)
-        //         );
-        //     });
+        if ( !x.role || roles.some(role => role.equals(x.role))) {
+            const filter = {};
+            Object.assign(filter,
+                x.access && convertFilter(x.access, Record, user)
+            );
+            console.log(JSON.stringify(filter));
+            permissionFilters.push(filter);
+        }
     });
 
-    const formAbility = new Ability(rules);
+    console.log(permissionFilters);
 
-    return Record.accessibleBy(formAbility).find(mongooseFilter)
+    return Record.find({ $and: [mongooseFilter, { $or: permissionFilters }] })
         .sort([[getSortField(sortField), sortOrder]])
         .skip(page * perPage)
         .limit(perPage);
