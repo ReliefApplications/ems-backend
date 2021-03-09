166f525ffc717ebf4cd4b4dc59c1bca9a3aac53a
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-03-09 10:41:40 +0200

    [SAFE-26] Few edit to have ejson

--- schema/query/recordsAggregation.ts
+++ schema/query/recordsAggregation.ts
@@ -1,15 +1,18 @@
 import { GraphQLNonNull } from "graphql";
 import GraphQLJSON from "graphql-type-json";
 import { Record } from "../../models";
+import { EJSON } from 'bson';
+
 export default {
     /* Take an aggregation configuration as parameter.
         Returns aggregated records data.
     */
     type: GraphQLJSON,
     args: {
-        aggregation: { type: new GraphQLNonNull(GraphQLJSON) },
+        pipeline: { type: new GraphQLNonNull(GraphQLJSON) },
     },
-    async resolve(parent, args, context) {
-        return Record.aggregate(args.aggregation);
+    resolve(parent, args, context) {
+        const pipeline = EJSON.deserialize(args.pipeline);
+        return Record.aggregate([pipeline]);
     }
 }
