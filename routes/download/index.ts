5695709ba11fc0b2c66221fdd5676392b28d0bf7
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-03-25 09:54:04 +0100

    [SAFE-193] csv from data working

--- routes/download/index.ts
+++ routes/download/index.ts
@@ -1,35 +1,16 @@
 import express from 'express';
 import { Form, Record } from '../../models';
-
+import csvBuilder from '../../utils/csvBuilder';
 
 const router = express.Router();
 router.get('/form/records/:id', async (req, res) => {
     const form = await Form.findById(req.params.id);
     const records = await Record.find({ form: req.params.id });
 
-    const headers = ['id', 'created at'].concat(form.fields.map(x => x.name));
-    console.log(headers);
-    const data = [
-        ['ID', 'Name', 'Age', 'Gender']
-        , [1, 'Taro Yamada', 25, 'Male']
-        , [2, 'Hanako Yamada', 24, 'Female']
-        , [3, 'John Doe', 30, 'Male']
-        , [4, 'Jane Doe', 30, 'Female']
-    ];
+    const fields = form.fields.map(x => x.name);
+    const data = records.map(x => x.data);
 
-    // CSV Specification
-    // http://www.ietf.org/rfc/rfc4180.txt
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/csv');
-
-    data.forEach((item) => {
-        res.write(item.map((field) => {
-            return '"' + field.toString().replace(/\"/g, '""') + '"';
-        }).toString() + '\r\n');
-    });
-
-    res.end();
+    return csvBuilder(res, form.name, fields, data);
 });
 
 export default router;
