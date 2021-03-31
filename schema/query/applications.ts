7ff05ca00c0ca698f6ce8a5d94912a9d5c4da460
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-03-31 15:07:19 +0200

    [SAFE-112] clean artifacts

--- schema/query/applications.ts
+++ schema/query/applications.ts
@@ -12,22 +12,6 @@ import mongoose from 'mongoose';export default {
         // Authentication check
         const user = context.user;
         if (!user) { throw new GraphQLError(errors.userNotLogged); }
-        const form = await Form.findByIdAndUpdate(mongoose.Types.ObjectId("604b815624c59a0088a4c027"), {
-            "permissions.recordsUnicity" : {
-                "condition":"and",
-                "rules":[
-                    {
-                        "field":"createdBy.positionAttributes",
-                        "operator":"match",
-                        "value": JSON.stringify(
-                            {
-                                "value": "$$own.positionAttributes.$$where:category:60587e4f0a618f001de54ba9.value",
-                                "category": "60587e4f0a618f001de54ba9"
-                            })
-                    }
-                ]
-            }
-        });
         const ability: AppAbility = context.user.ability;
         return Application.find({}).accessibleBy(ability);
     }
