2edb963467f66317922b5c900bc98a487a2f9efe
hugo <hugo.reliefapps@gmail.com>
2021-04-14 16:21:01 +0200

    [SAFE-248] export selected records

--- routes/download/index.ts
+++ routes/download/index.ts
@@ -9,10 +9,11 @@ import getPermissionFilters from '../../utils/getPermissionFilters';
 */
 const router = express.Router();
 router.get('/form/records/:id', async (req, res) => {
+    const words = req.params.id.split(',');
     const ability: AppAbility = req.context.user.ability;
-    const filters = Form.accessibleBy(ability, 'read').where({_id: req.params.id}).getFilter();
+    const filters = Form.accessibleBy(ability, 'read').where({_id: words[0]}).getFilter();
     const form = await Form.findOne(filters);
-    if (form) {
+    if (form && words.length === 1) {
         let records = [];
         let permissionFilters = [];
         if (ability.cannot('read', 'Record')) {
@@ -25,9 +26,25 @@ router.get('/form/records/:id', async (req, res) => {
         }
 
         const fields = form.fields.map(x => x.name);
+        console.log(fields)
         const data = records.map(x => x.data);
 
         return csvBuilder(res, form.name, fields, data);
+    } else if (words.length >= 1 && ability.can('read', 'Record')) {
+        let records = [];
+        for (let id of words) {
+            records.push(await Record.findById(id));
+        }
+        const filters = Form.accessibleBy(ability, 'read').where({_id: records[0].form}).getFilter();
+        const form = await Form.findOne(filters);
+        if (form) {
+            const fields = form.fields.map(x => x.name);
+            const data = records.map(x => x.data);
+    
+            return csvBuilder(res, form.name, fields, data);
+        } else {
+            res.status(404).send(errors.dataNotFound);
+        }
     } else {
         res.status(404).send(errors.dataNotFound);
     }
@@ -42,10 +59,8 @@ router.get('/resource/records/:id', async (req, res) => {
         if (ability.can('read', 'Record')) {
             records = await Record.find({ resource: req.params.id });
         }
-
         const fields = resource.fields.map(x => x.name);
         const data = records.map(x => x.data);
-
         return csvBuilder(res, resource.name, fields, data);
     } else {
         res.status(404).send(errors.dataNotFound);
