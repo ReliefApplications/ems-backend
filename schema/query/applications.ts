0f6691ab25cce09c7643e3f2c4f57ddb00a51a2b
adrianmujica <adrian@reliefapplications.org>
2021-05-13 11:02:18 +0100

    [SAFE-286] feat: support filters on get applications

--- schema/query/applications.ts
+++ schema/query/applications.ts
@@ -3,27 +3,64 @@ import { ApplicationType } from "../types";
 import { Application } from "../../models";
 import errors from "../../const/errors";
 import { AppAbility } from "../../security/defineAbilityFor";
-
+import GraphQLJSON from "graphql-type-json";
 
 export default {
     /*  List all applications available for the logged user.
         Throw GraphQL error if not logged.
     */
     type: new GraphQLList(ApplicationType),
-    args : {
-        page: { type: GraphQLInt },
-        perPage: { type: GraphQLInt }
+    args: {
+        page: {type: GraphQLInt},
+        perPage: {type: GraphQLInt},
+        filters: {type: GraphQLJSON}
     },
     async resolve(parent, args, context) {
         // Authentication check
         const user = context.user;
-        if (!user) { throw new GraphQLError(errors.userNotLogged); }
+        if (!user) {
+            throw new GraphQLError(errors.userNotLogged);
+        }
         const ability: AppAbility = context.user.ability;
+
+        let filters = buildFilters(args.filters);
+
         if (args.page === 0 || args.page) {
-            if (!args.perPage) { throw new GraphQLError(errors.invalidGetApplicationsArguments); }
-            return Application.find({}).accessibleBy(ability).skip(args.page * args.perPage).limit(args.perPage);
+            if (!args.perPage) {
+                throw new GraphQLError(errors.invalidGetApplicationsArguments);
+            }
+            return Application.find(filters).accessibleBy(ability).skip(args.page * args.perPage).limit(args.perPage).sort({createdAt: 1});
         } else {
-            return Application.find({}).accessibleBy(ability);
+            return Application.find(filters).accessibleBy(ability).sort({createdAt: 1});
         }
     }
+}
+
+function buildFilters(filters) {
+    if (filters) {
+        const conditions = [];
+
+        if (filters.name && filters.name.trim().length > 0) {
+            conditions.push({name: {$regex: new RegExp(filters.name, 'i')}});
+        }
+        if (filters.dateRange.start.trim().length > 0 && filters.dateRange.end.trim().length > 0) {
+            conditions.push({
+                createdAt: {
+                    $gte: new Date(filters.dateRange.start),
+                    $lte: new Date(filters.dateRange.end)
+                }
+            })
+        }
+
+        if (filters.status && filters.status.trim().length > 0) {
+            conditions.push({status: {$regex: filters.status}});
+        }
+
+        if (conditions.length > 0) {
+            return {
+                $and: conditions
+            };
+        }
+    }
+    return {};
 }
