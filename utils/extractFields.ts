680ff81c6d30e9675245373fd7b26053855b4629
Pacôme Rivier <pacome@reliefapplications.org>
2021-05-14 12:14:04 +0200

    [SAFE-295] Fix resource inheritance and protection of core fields

--- utils/extractFields.ts
+++ utils/extractFields.ts
@@ -5,11 +5,11 @@ import errors from '../const/errors';
 /*  Push in fields array all detected fields in the json structure of object.
     Function by induction.
 */
-async function extractFields(object, fields) {
+async function extractFields(object, fields, core) {
     if (object.elements) {
         for (const element of object.elements) {
             if (element.type === 'panel') {
-                await extractFields(element, fields);
+                await extractFields(element, fields, core);
             } else {
                 if (!element.valueName) {
                     throw new GraphQLError(errors.missingDataField);
@@ -19,7 +19,8 @@ async function extractFields(object, fields) {
                     type,
                     name: element.valueName,
                     isRequired: element.isRequired ? element.isRequired : false,
-                    readOnly: element.readOnly ? element.readOnly : false
+                    readOnly: element.readOnly ? element.readOnly : false,
+                    isCore: core
                 };
                 // ** Resource **
                 if (element.type === 'resource' || element.type === 'resources') {
