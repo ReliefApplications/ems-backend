b1bf4c690c37a7b9b37d8acdb16edd680d2f8d31
MartinGbz <martin.grabarz@outlook.fr>
2021-06-16 11:51:00 +0200

    [SAFE-374] code cleaning

--- utils/updateRecords.ts
+++ utils/updateRecords.ts
@@ -2,23 +2,6 @@ import request from "request"
 
 export default async (form: any) => {
 
-    console.log("***");
-    console.log(form);
-    console.log("***");
-    console.log(form.fields);
-    console.log("***");
-    for (const e of form.fields){
-        console.log(e.type);
-    }
-    console.log("***");
-
-    // a2MN6zEzV6pXMbY3Jx7iCr
-    // aK3TovsSRkTZJPtgNXwGEQ
-    // atNAFqYhKhkyDce3eN5CQq
-
-    // aN5SzmYrAiPWJhi4CJoGSW
-
-
     const options = {
         'method': 'GET',
         'url': 'https://kobo.humanitarianresponse.info/assets/aN5SzmYrAiPWJhi4CJoGSW/submissions/?format=json',
@@ -51,65 +34,45 @@ export default async (form: any) => {
                 for (const [key, value] of Object.entries(records[r])){
                     val = value;
                     if( q.name == key || q.name ==  key.toString().split('/')[0]){
-                        // for (const e of form.fields){
-                            // if the element is normal
-                            if(q.name == key){
-                                if(q.type == 'tagbox' || q.type == 'checkbox'){
-                                    val = value.toString().split(" ");
-                                }
+                        // if the element is normal
+                        if(q.name == key){
+                            if(q.type == 'tagbox' || q.type == 'checkbox'){
+                                val = value.toString().split(" ");
                             }
-                            // if the element is a group (the name pattern is something/something)
-                            else if(q.name == key.toString().split('/')[0]){
-                                if( q.type == 'multipletext'){
-                                    let arrTemp = [];
-                                    if (Array.isArray(recordsToImport[r][key.toString().split('/')[0]])) {
-                                        arrTemp = recordsToImport[r][key.toString().split('/')[0]];
-                                        arrTemp.push(value);
-                                    } else {
-                                        arrTemp.push(value);
-                                    }
-                                    val = arrTemp;
-                                    // console.log(val);
+                        }
+                        // if the element is a group (the name pattern is something/something)
+                        else if(q.name == key.toString().split('/')[0]){
+                            if( q.type == 'multipletext'){
+                                let arrTemp = [];
+                                if (Array.isArray(recordsToImport[r][key.toString().split('/')[0]])) {
+                                    arrTemp = recordsToImport[r][key.toString().split('/')[0]];
                                 }
-                                if( q.type == 'matrix'){
-                                    const n = key.toString().split('/')[1];
-                                    if(recordsToImport[r][q.name] == null){
-                                        recordsToImport[r][q.name] = {};
-                                    }
-                                    recordsToImport[r][q.name][n] = value;
+                                arrTemp.push(value);
+                                val = arrTemp;
+                            }
+                            if( q.type == 'matrix'){
+                                const n = key.toString().split('/')[1];
+                                if(recordsToImport[r][q.name] == null){
+                                    recordsToImport[r][q.name] = {};
                                 }
-                                if( q.type == 'matrixdropdown'){
-                                    const n = key.toString().split('/')[1];
-                                    // const n2 = n.split('_',1)[1];
-                                    const n2 = n.substr(n.indexOf('_')+1);
-                                    // console.log(n2);
-                                    if(recordsToImport[r][q.name] == null){
-                                        // console.log(n2 + ' ' + 'null');
-                                        recordsToImport[r][q.name] = {};
-                                    }
-                                    console.log('BEFORE');
-                                    console.log(recordsToImport[r][q.name]);
-                                    console.log(n2);
-                                    if(recordsToImport[r][q.name][n2] == null){
-                                        console.log('AFTER');
-                                        // console.log(n2 + ' ' + 'null 2');
-                                        // console.log(recordsToImport[r][q.name][n2]);
+                                recordsToImport[r][q.name][n] = value;
+                            }
+                            if( q.type == 'matrixdropdown'){
+                                const n = key.toString().split('/')[1];
+                                const n2 = n.substr(n.indexOf('_')+1);
 
-                                        // console.log('n4');
-                                        // console.log(n4);
-                                        recordsToImport[r][q.name][n2] = {};
-                                    }
-                                    const n3 = key.toString().split('/')[2];
-                                    // console.log('n3');
-                                    // console.log(n3);
-                                    const n4 = n3.split(n+'_')[1];
-                                    recordsToImport[r][q.name][n2][n4] = value;
-                                    console.log(recordsToImport[r][q.name]);
-                                    // recordsToImport[r][q.name][n2] = value;
-                                    // console.log(recordsToImport[r][q.name][n2]);
+                                if(recordsToImport[r][q.name] == null){
+                                    recordsToImport[r][q.name] = {};
                                 }
+
+                                if(recordsToImport[r][q.name][n2] == null){
+                                    recordsToImport[r][q.name][n2] = {};
+                                }
+                                const n3 = key.toString().split('/')[2];
+                                const n4 = n3.split(n+'_')[1];
+                                recordsToImport[r][q.name][n2][n4] = value;
                             }
-                        // }
+                        }
                         if( q.type != 'matrix' && q.type != 'matrixdropdown' )
                             recordsToImport[r][q.name] = val;
                     }
@@ -117,6 +80,5 @@ export default async (form: any) => {
             }
         }
         console.log(recordsToImport);
-        console.log(recordsToImport[0]['q11']);
     });
 }
