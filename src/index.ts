2884c7e7db6ec84d5d75ec3f9b834ab738bc0f6b
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-06-16 12:33:03 +0200

    [SAFE-38] redo package json

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { SafeServer } from './server';
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Express {
+        interface Request {
+            context: any;
+        }
+    }
+}
+
+const PORT = 3000;
+
+const safeServer = new SafeServer();
+
+safeServer.status.on('ready', () => {
+    safeServer.httpServer.listen(PORT, () => {
+        console.log(`🚀 Server ready at http://localhost:${PORT}${safeServer.apolloServer.graphqlPath}`);
+        console.log(`🚀 Server ready at ws://localhost:${PORT}${safeServer.apolloServer.subscriptionsPath}`);
+    });
+})
