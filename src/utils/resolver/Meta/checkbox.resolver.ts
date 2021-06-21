74261e2954908ee580dd8d7fc22b391d717da134
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-06-21 12:55:09 +0200

    fix choices if choicesbyurl for meta resolvers

--- src/utils/resolver/Meta/checkbox.resolver.ts
+++ src/utils/resolver/Meta/checkbox.resolver.ts
@@ -1,11 +1,13 @@
 function checkboxMeta (field) {
     return Object.assign(field, {
-        choices: field.choices.map(x => {
-            return {
-                text: x.text ? x.text : x,
-                value: x.value ? x.value : x
-            };
-        })
+        ...field.choices && { 
+            choices: field.choices.map(x => {
+                return {
+                    text: x.text ? x.text : x,
+                    value: x.value ? x.value : x
+                };
+            })
+        }
     });
 }
 
