395e7ec74a7bfb4c46bd4b2e612df0766ae6bd41
MartinGbz <martin.grabarz@outlook.fr>
2021-06-23 13:09:04 +0200

    [SAFE-399] uid undefined request fixed (with retry)

--- utils/exportForms.ts
+++ utils/exportForms.ts
@@ -4,95 +4,118 @@ import {AppAbility} from "../security/defineAbilityFor";
 import koboBuilder from "./files/koboBuilder";
 import errors from "../const/errors";
 
+let uid1;
+let uid2;
+let accessToken;
+let finalRes;
+let form;
+let buffer;
+
 export default async (req: any, res: any) => {
     const ability: AppAbility = req.context.user.ability;
     const filters = Form.accessibleBy(ability, 'read').where({_id: req.params.id}).getFilter();
-    const form = await Form.findOne(filters);
-    const accessToken = req.body.aToken;
-    let buffer;
-    let uid1;
-    let uid2;
+    form = await Form.findOne(filters);
+    accessToken = req.body.aToken;
+    finalRes = res;
+
     if (form) {
-
-        // CREATE EXCEL FILE
-        console.log('CREATE EXCEL FILE');
-        buffer = await koboBuilder(form);
-        // console.log(form);
-
-        // IMPORT EXCEL FILE
-        console.log('IMPORT EXCEL FILE');
-        const options = {
-            'method': 'POST',
-            'url': 'https://kobo.humanitarianresponse.info/api/v2/imports/?format=json',
-            'headers': {
-                'Authorization':`Token ${accessToken}`,
-                'Accept': 'application/json',
-            },
-            formData: {
-                'file': {
-                    'value': buffer,
-                    'options': {
-                        'filename': 'test_import.xlsx',
-                        'contentType': null
-                    }
-                },
-                'library': 'false',
-                'name': 'form_'+Date.now().toString(),
-            }
-        };
-        await request(options, function (error, response) {
-            if (error) throw new Error(error);
-            const body = JSON.parse(response.body.toString());
-            uid1 = body.uid;
-            console.log('uid1');
-            console.log(uid1);
-
-            // GET UID OF THE NEW FORM
-            console.log('GET UID OF THE NEW FORM');
-            // have to put a timer, otherwise the request status is still in processing (instead of complete)
-            // 5 seconds is enough
-            setTimeout(() => {
-                const options = {
-                    'method': 'GET',
-                    'url': `https://kobo.humanitarianresponse.info/api/v2/imports/${uid1}/?format=json`,
-                    'headers': {
-                        'Authorization': `Token ${accessToken}`,
-                        'Accept': 'application/json',
-                    }
-                };
-                request(options, function (error, response) {
-                    if (error) throw new Error(error);
-                    const body = JSON.parse(response.body.toString());
-                    console.log('body');
-                    console.log(body);
-                    uid2 = body.messages.created[0].uid;
-                    console.log('*** uid2 ***');
-                    console.log(uid2);
-
-                    // DEPLOY FORM
-                    console.log('DEPLOY FORM');
-                    const options = {
-                        'method': 'POST',
-                        'url': `https://kobo.humanitarianresponse.info/api/v2/assets/${uid2}/deployment/?format=json`,
-                        'headers': {
-                            'Authorization': `Token ${accessToken}`,
-                        },
-                        formData: {
-                            'active': 'true'
-                        }
-                    };
-                    request(options, function (error, response) {
-                        if (error) throw new Error(error);
-                        console.log(response.body);
-                        const body = JSON.parse(response.body.toString());
-                        const url = body.asset.deployment__links.url;
-                        res.send({url: url});
-                    });
-                });
-            }, 5000);
-        });
-
+        await importFormAnd2More();
     } else {
         res.status(404).send(errors.dataNotFound);
     }
+}
+
+async function importFormAnd2More() {
+    // CREATE EXCEL FILE
+    console.log('CREATE EXCEL FILE');
+    buffer = await koboBuilder(form);
+    // console.log(form);
+
+    // IMPORT EXCEL FILE
+    console.log('IMPORT EXCEL FILE');
+    const options = {
+        'method': 'POST',
+        'url': 'https://kobo.humanitarianresponse.info/api/v2/imports/?format=json',
+        'headers': {
+            'Authorization': `Token ${accessToken}`,
+            'Accept': 'application/json',
+        },
+        formData: {
+            'file': {
+                'value': buffer,
+                'options': {
+                    'filename': 'test_import.xlsx',
+                    'contentType': null
+                }
+            },
+            'library': 'false',
+            'name': 'form_' + Date.now().toString(),
+        }
+    };
+    await request(options, function (error, response) {
+        if (error) throw new Error(error);
+        const body = JSON.parse(response.body.toString());
+        uid1 = body.uid;
+        console.log('uid1');
+        console.log(uid1);
+        getFormUidAnd1more();
+
+    });
+}
+
+function getFormUidAnd1more() {
+    // GET UID OF THE NEW FORM
+    console.log('GET UID OF THE NEW FORM');
+    // have to put a timer, otherwise the request status is still in processing (instead of complete)
+
+    const options = {
+        'method': 'GET',
+        'url': `https://kobo.humanitarianresponse.info/api/v2/imports/${uid1}/?format=json`,
+        'headers': {
+            'Authorization': `Token ${accessToken}`,
+            'Accept': 'application/json',
+        }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        const body = JSON.parse(response.body.toString());
+        if(Object.keys(body.messages).length != 0) {
+            console.log('body');
+            console.log(body);
+            uid2 = body.messages.created[0].uid;
+            console.log('*** uid2 ***');
+            console.log(uid2);
+
+            deployForm();
+        }
+        else {
+            setTimeout( () => {
+                console.log('retry request');
+                getFormUidAnd1more();
+            }, 5000);
+        }
+    });
+}
+
+function deployForm(){
+    // DEPLOY FORM
+    console.log('DEPLOY FORM');
+    const options = {
+        'method': 'POST',
+        'url': `https://kobo.humanitarianresponse.info/api/v2/assets/${uid2}/deployment/?format=json`,
+        'headers': {
+            'Authorization': `Token ${accessToken}`,
+        },
+        formData: {
+            'active': 'true'
+        }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        // if(response)
+        console.log(response.body);
+        const body = JSON.parse(response.body.toString());
+        const url = body.asset.deployment__links.url;
+        finalRes.send({url: url});
+    });
 }
