a6eb390205880d66c9a91eda4f412ae8d32d878c
Pacôme Rivier <pacome@reliefapplications.org>
2021-07-01 09:54:00 +0200

    [SAFE-357] Add type to context

--- src/server/apollo/context.ts
+++ src/server/apollo/context.ts
@@ -1,13 +1,24 @@
-export default async ({ req, connection }) => {
+import { User } from 'models'
+import { AppAbility } from 'security/defineAbilityFor';
+
+interface Context {
+    user: UserWithAbility;
+}
+
+interface UserWithAbility extends User {
+    ability: AppAbility;
+}
+
+export default ({ req, connection }): Context => {
     if (connection) {
         return {
             user: connection.context.user,
-        };
+        } as Context;
     }
     if (req) {
         return {
             // not a clean fix but that works for now
             user: (req as any).user
-        };
+        } as Context;
     }
 }
