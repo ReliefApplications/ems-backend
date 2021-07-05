22a3ff5834d4baa220e9103730d91011227de25d
Pacôme Rivier <pacome@reliefapplications.org>
2021-07-05 16:05:53 +0200

    [SAFE-387] Add an input type for users

--- src/schema/inputs/user.input.ts
+++ src/schema/inputs/user.input.ts
@@ -1,8 +1,11 @@
-import { GraphQLInputObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
+import { GraphQLID, GraphQLInputObjectType, GraphQLList, GraphQLNonNull, GraphQLString } from 'graphql';
+import { PositionAttributeInputType } from './position-attribute';
 
 export const UserInputType = new GraphQLInputObjectType({
     name: 'UserInputType',
     fields: () => ({
-        email: { type: new GraphQLNonNull(GraphQLString) }
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        roles: { type: new GraphQLNonNull( new GraphQLList(GraphQLID)) },
+        attributes: { type: new GraphQLList(PositionAttributeInputType) }
     })
 });
