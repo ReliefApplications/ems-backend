88114e0f3184f899f419971be2e1a2d35512e5b7
adrianmujica <adrian@reliefapplications.org>
2021-07-09 15:07:48 +0100

    [SAFE-418] avoid store the file and read directly from data buffer

--- src/routes/upload/index.ts
+++ src/routes/upload/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { Workbook } from 'exceljs';
 import { Form, Record } from "../../models";
 
+const FILE_SIZE_LIMIT = 5 * 1024 * 1024;
+
 const router = express.Router();
 /**
  * Upload file with data
@@ -22,7 +24,7 @@ router.post('/form/records/:id', async (req: any, res) => {
     const file = req.files.sampleFile;
 
     // Check file size
-    if (file.size > 5 * 1024 * 1024) {
+    if (file.size > FILE_SIZE_LIMIT) {
         return res.status(400).send('File size exceed 5MB');
     }
 
@@ -31,47 +33,37 @@ router.post('/form/records/:id', async (req: any, res) => {
         return res.status(400).send('Invalid extension');
     }
 
+    const records: Record[] = [];
+
     const workbook = new Workbook();
-
-    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-    const uploadPath = __dirname + '/' + file.name;
-
-    // Use the mv() method to place the file somewhere on your server
-    file.mv(uploadPath, function (err) {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        const records: Record[] = [];
-        workbook.xlsx.readFile(uploadPath)
-            .then(function () {
-                let keys = [];
-                const worksheet = workbook.getWorksheet(1);
-                worksheet.eachRow({includeEmpty: false}, function (row, rowNumber) {
-                    const values = Object.values(row.values);
-                    if (rowNumber === 1) {
-                        keys = values;
-                    } else {
-                        const data = {};
-                        keys.forEach((key, index) => {
-                            data[`${key}`] = values[index];
-                        });
-                        records.push(new Record({
-                            form: form.id,
-                            createdAt: new Date(),
-                            modifiedAt: new Date(),
-                            data: data,
-                            resource: form.resource ? form.resource : null
-                        }));
-                    }
+    workbook.xlsx.load(file.data).then((wb) => {
+        let keys = [];
+        const worksheet = workbook.getWorksheet(1);
+        worksheet.eachRow({includeEmpty: false}, function (row, rowNumber) {
+            const values = Object.values(row.values);
+            if (rowNumber === 1) {
+                keys = values;
+            } else {
+                const data = {};
+                keys.forEach((key, index) => {
+                    data[`${key}`] = values[index];
                 });
-                if (records.length > 0) {
-                    Record.insertMany(records, {}, async () => {
-                        return res.status(200).send({status: 'OK'});
-                    });
-                } else {
-                    return res.status(200).send({status: ''});
-                }
+                records.push(new Record({
+                    form: form.id,
+                    createdAt: new Date(),
+                    modifiedAt: new Date(),
+                    data: data,
+                    resource: form.resource ? form.resource : null
+                }));
+            }
+        });
+        if (records.length > 0) {
+            Record.insertMany(records, {}, async () => {
+                return res.status(200).send({status: 'OK'});
             });
+        } else {
+            return res.status(200).send({status: ''});
+        }
     });
 });
 
