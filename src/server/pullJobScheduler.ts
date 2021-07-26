70e78ae39a53d2eb0339266749ab5ccce5d89172
Pacôme Rivier <pacome@reliefapplications.org>
2021-07-26 16:12:27 +0200

    Deal with flatMap for pullJob mapping

--- src/server/pullJobScheduler.ts
+++ src/server/pullJobScheduler.ts
@@ -226,7 +226,7 @@ function accessFieldIncludingNested(data: any, identifier: string) {
         for (const field of fields) {
             if (value) {
                 if (Array.isArray(value) && isNaN(field)) {
-                    value = value.map(x => x ? x[field] : null);
+                    value = value.flatMap(x => x ? x[field] : null);
                 } else {
                     value = value[field];
                 }
