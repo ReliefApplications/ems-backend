349ed3fbc6a87e45201432a93a32804c3e660d5d
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-09-14 11:49:52 +0200

    fix: CICD

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -12,7 +12,7 @@ CMD="'""cd $REMOTE_PATH && echo '$SSH_PASS' | sudo -S docker-compose pull api""'
 ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
 
 echo -e "Stopping docker containers..."
-CMD="'""cd $REMOTE_PATH && echo '$SSH_PASS' | sudo -S docker-compose pull up -d --no-deps api""'"
+CMD="'""cd $REMOTE_PATH && echo '$SSH_PASS' | sudo -S docker-compose up -d --no-deps api""'"
 ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
 
 echo -e "Deployed!"
