09d8fe1094714238dd9768af1413c2bfc9ea07bd
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-09-20 13:23:21 +0200

    remove name from addstep

--- src/schema/mutation/addStep.ts
+++ src/schema/mutation/addStep.ts
@@ -1,7 +1,7 @@
 import { GraphQLString, GraphQLNonNull, GraphQLID, GraphQLError } from 'graphql';
 import { contentType } from '../../const/enumTypes';
 import errors from '../../const/errors';
-import { Workflow, Dashboard, Step, Page, Application, Role } from '../../models';
+import { Workflow, Dashboard, Step, Page, Application, Role, Form } from '../../models';
 import { StepType } from '../types';
 import mongoose from 'mongoose';
 import { AppAbility } from '../../security/defineAbilityFor';
@@ -13,7 +13,6 @@ export default {
     */
     type: StepType,
     args: {
-        name: { type: GraphQLString },
         type: { type: new GraphQLNonNull(GraphQLString) },
         content: { type: GraphQLID },
         workflow: { type: new GraphQLNonNull(GraphQLID) }
@@ -30,22 +29,30 @@ export default {
             const page = await Page.findOne({ content: args.workflow });
             if (!page) { throw new GraphQLError(errors.dataNotFound); }
             const application = await Application.findOne({ pages: { $elemMatch: { $eq: mongoose.Types.ObjectId(page._id) }}});
+            let stepName = '';
             if (ability.can('update', application)) {
                 const workflow = await Workflow.findById(args.workflow);
                 if (!workflow) throw new GraphQLError(errors.dataNotFound);
                 // Create a linked Dashboard if necessary
                 if (args.type === contentType.dashboard) {
+                    stepName = 'Dashboard';
                     const dashboard = new Dashboard({
-                        name: args.name,
+                        name: stepName,
                         createdAt: new Date(),
                     });
                     await dashboard.save();
                     args.content = dashboard._id;
+                } else {
+                    const form = await Form.findById(args.content);
+                    if (!form) {
+                        throw new GraphQLError(errors.dataNotFound);
+                    }
+                    stepName = form.name;
                 }
                 // Create a new step.
                 const roles = await Role.find({ application: application._id });
                 const step = new Step({
-                    name: args.name,
+                    name: stepName,
                     createdAt: new Date(),
                     type: args.type,
                     content: args.content,
