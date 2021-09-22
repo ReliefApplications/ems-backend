940c92667fc6deb0135b988b4ee48693bc21a017
Antoine Hurard <antoine.reliefapps@gmail.com>
2021-09-22 16:45:25 +0200

    [SAFE-501] add matrix / matrixdynamic / multipletext export

--- src/utils/files/getRows.ts
+++ src/utils/files/getRows.ts
@@ -16,6 +16,37 @@ export const getRows = (columns: any[], records: any[]): any[] => {
                 case 'checkbox': {
                     const value = data[column.field]?.includes(column.value) ? 1 : 0;
                     set(row, column.name, value);
+                    break;
+                }
+                case 'tagbox': {
+                    const value = data[column.field]?.includes(column.value) ? 1 : 0;
+                    set(row, column.name, value);
+                    break;
+                }
+                case 'multipletext': {
+                    console.log('okokok')
+                    console.log(column.field);
+                    console.log(column.name);
+                    console.log(data[column.name]);
+                    const value = get(data, column.name);
+                    console.log(value)
+                    set(row, column.name, value);
+                    break;
+                }
+                case 'matrix': {
+                    const value = get(data, column.name);
+                    set(row, column.name, value);
+                    break;
+                }
+                case 'matrixdropdown': {
+                    const value = get(data, column.name);
+                    set(row, column.name, value);
+                    break;
+                }
+                default: {
+                    const value = data[column.field];
+                    set(row, column.name, value);
+                    break;
                 }
             }
         }
