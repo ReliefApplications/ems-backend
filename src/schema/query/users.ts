3ef1455b7e24eb0832f71cf1a3a63988d8488de1
Antoine Hurard <50317271+AntoineRelief@users.noreply.github.com>
2021-09-27 16:19:58 +0200

    Revert "[SAFE-181]"

--- src/schema/query/users.ts
+++ src/schema/query/users.ts
@@ -13,6 +13,7 @@ export default {
         // Authentication check
         const user = context.user;
         if (!user) { throw new GraphQLError(errors.userNotLogged); }
+
         const ability: AppAbility = context.user.ability;
         if (ability.can('read', 'User')) {
             return User.find({}).populate({
