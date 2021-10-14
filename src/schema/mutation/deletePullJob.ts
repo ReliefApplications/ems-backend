0fb71b4f435b663fe0f86d3efbf7ab70bdee07d0
Matthis M <matthis.reliefapps@gmail.com>
2021-10-14 11:33:09 +0200

    Unbound pullJobs to applications

--- src/schema/mutation/deletePullJob.ts
+++ src/schema/mutation/deletePullJob.ts
@@ -1,7 +1,7 @@
 import { GraphQLError, GraphQLID, GraphQLNonNull } from 'graphql';
 import { PullJobType } from '../types';
 import errors from '../../const/errors';
-import { Application, PullJob} from '../../models';
+import { PullJob} from '../../models';
 import { AppAbility } from '../../security/defineAbilityFor';
 import { unscheduleJob } from '../../server/pullJobScheduler';
 
@@ -10,7 +10,6 @@ export default {
     */
     type: PullJobType,
     args: {
-        application: { type: new GraphQLNonNull(GraphQLID) },
         id: { type: GraphQLNonNull(GraphQLID) },
     },
     async resolve(parent, args, context) {
@@ -19,8 +18,6 @@ export default {
             throw new GraphQLError(errors.userNotLogged);
         }
         const ability: AppAbility = user.ability;
-        const application = await Application.findById(args.application);
-        if (!application) throw new GraphQLError(errors.dataNotFound);
 
         const filters = PullJob.accessibleBy(ability, 'delete').where({_id: args.id}).getFilter();
         const pullJob = await PullJob.findOneAndDelete(filters);
@@ -30,11 +27,6 @@ export default {
             $pull: { pullJobs: pullJob.id },
         };
         
-        await Application.findByIdAndUpdate(
-            args.application,
-            update,
-            { new: true }
-        );
         unscheduleJob(pullJob);
         return pullJob;
     }
