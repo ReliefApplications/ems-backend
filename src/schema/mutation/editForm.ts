00fc8aa8b21de69e51e411cbcb8958b493f1a2e2
Matthis M <matthis.reliefapps@gmail.com>
2021-10-27 17:46:14 +0200

    !WIP! filters changes inheritance on children #11558

--- src/schema/mutation/editForm.ts
+++ src/schema/mutation/editForm.ts
@@ -11,6 +11,7 @@ import errors from '../../const/errors';
 import { AppAbility } from '../../security/defineAbilityFor';
 import { status, StatusEnumType } from '../../const/enumTypes';
 import isEqual from 'lodash/isEqual';
+import _ from 'lodash';
 
 
 export default {
@@ -52,16 +53,17 @@ export default {
                 for (const field of fields.filter(x => ['resource', 'resources'].includes(x.type))) {
                     // Raises an error if the field is already used as related name for this resource
                     if (await Form.findOne({
-                            fields: { $elemMatch: { resource: field.resource, relatedName: field.relatedName } },
-                            _id: { $ne: form.id },
-                            ...form.resource && { resource: { $ne: form.resource } }
-                        })) {
+                        fields: { $elemMatch: { resource: field.resource, relatedName: field.relatedName } },
+                        _id: { $ne: form.id },
+                        ...form.resource && { resource: { $ne: form.resource } }
+                    })) {
                         throw new GraphQLError(errors.relatedNameDuplicated(field.relatedName));
                     }
                     // Raises an error if the field exists in the resource
                     if (await Resource.findOne({
                         fields: { $elemMatch: { name: field.relatedName } },
-                        _id: field.resource })
+                        _id: field.resource
+                    })
                     ) {
                         throw new GraphQLError(errors.relatedNameDuplicated(field.relatedName));
                     }
@@ -74,28 +76,27 @@ export default {
                 const oldFields: any[] = JSON.parse(JSON.stringify(resource.fields));
                 const usedFields = childForms.map(x => x.fields).flat().concat(fields);
                 // Check fields against the resource to add new ones or edit old ones
-                for (const field of fields) {
-                    const oldField = oldFields.find((x) => x.name === field.name);
-                    if (!oldField) {
-                        // If the field is not in the resource add a new one
-                        const newField: any = Object.assign({}, field);
-                        newField.isRequired = form.core && field.isRequired ? true : false;
-                        oldFields.push(newField);
+                for (const field of fields) { // For each field in the form being saved
+                    const oldField = oldFields.find((x) => x.name === field.name); // Find the equivalent field in the ressource's fields
+                    if (!oldField) { // If the field isn't found in the resource
+                        const newField: any = Object.assign({}, field); // Create a copy of the form's field
+                        newField.isRequired = form.core && field.isRequired ? true : false; // If it's a core form and the field isRequired, copy this property
+                        oldFields.push(newField); // Add this field to the list of the resource's fields
                     } else {
                         // Check if field can be updated
-                        if (!oldField.isCore || oldField.isCore && form.core) {
-                            // Check if the field has changes
-                            if (!isEqual(oldField, field)) {
-                                const index = oldFields.findIndex((x) => x.name === field.name);
-                                oldFields.splice(index, 1, field);
+                        if (!oldField.isCore || oldField.isCore && form.core) { //If resource's field isn't core or if it's core but the edited form is core too, make it writable
+                            if (!isEqual(oldField, field)) { // If the resource's field and the current form's field are different
+                                const index = oldFields.findIndex((x) => x.name === field.name); // Get the index of the form's field in the resources
+                                oldFields.splice(index, 1, field); // Replace resource's field by the form's field
                                 // === REFLECT UPDATE ===
-                                for (const childForm of childForms) {
-                                    // Update field
-                                    childForm.fields = childForm.fields.map(x => x.name === field.name ? field : x);
+                                for (const childForm of childForms) { // For each form that inherits from the same resource
+
+                                    childForm.fields = childForm.fields.map(x => x.name === field.name ? field : x); // If there's a field sharing the same name in the childForm, replace it by the new field -- TODO Can't this be optimized ?
+
                                     // Update structure
-                                    const newStructure = JSON.parse(childForm.structure);
-                                    replaceField(newStructure, field.name, structure);
-                                    childForm.structure = JSON.stringify(newStructure)
+                                    const newStructure = JSON.parse(childForm.structure); // Get the inheriting form's structure
+                                    replaceField(newStructure, field.name, structure); // Replace the inheriting form's field by the edited form's field 
+                                    childForm.structure = JSON.stringify(newStructure) // Save the new structure
                                     // Update form
                                     const update = {
                                         structure: childForm.structure,
@@ -107,23 +108,28 @@ export default {
                         }
                     }
                 }
+
                 // Check if there are unused or duplicated fields in the resource
                 for (let index = 0; index < oldFields.length; index++) {
-                    const field = oldFields[index];
-                    const removeField = (form.core ? !fields.some(x => x.name === field.name) : true) && !usedFields.some(x => x.name === field.name) // Unused
-                        || oldFields.some((x, id) => field.name === x.name && id !== index) // Duplicated
+                    const field = oldFields[index]; // Store the resource's field
+                    const removeField =
+                        (form.core ? !fields.some(x => x.name === field.name) : true) // If edited form is core, check if resource's field is absent from form's fields
+                        &&
+                        !usedFields.some(x => x.name === field.name) // Unused -- TODO What if it's in one inherited form and not in another ?
+                        ||
+                        oldFields.some((x, id) => field.name === x.name && id !== index) // Duplicated If there's another field with the same name but not the same ID
                     if (removeField) {
                         oldFields.splice(index, 1);
-                        index --;
+                        index--;
                     }
                 }
                 if (!form.core) {
                     // Check if a required field is missing
                     // Keep old version and move that to extract fields ? 
                     let fieldExists = false;
-                    for (const field of oldFields.filter(x => x.isCore)) {
+                    for (const field of oldFields.filter(x => x.isCore)) { // For each non-core field in the resource
                         fields.map(x => {
-                            if (x.name === field.name) {
+                            if (x.name === field.name) { // If 
                                 x.isCore = true;
                                 fieldExists = true;
                             }
@@ -185,14 +191,112 @@ export default {
                         }
                     }
                 }
-                
+
                 // Update resource fields
                 await Resource.findByIdAndUpdate(form.resource, {
                     fields: oldFields,
                 });
             }
             update.fields = fields;
+
+            let testArray: any[] = undefined;
+            // console.log(testArray.filter((e)=>(true))) £// throws
+            // _.isEqual(null, undefined) // returns false
+
+            if (form.core) {
+                let formStructure = JSON.parse(form.structure);
+                //let newStructure = structure;
+                let removedTriggers: any[];
+
+                /*
+                formStructure.triggers = [{
+                    type: 'setvalue',
+                    expression: '{please_add_a_random_number} > 10',
+                    setToName: 'this_is_the_random_number_you_entered',
+                    setValue: 'It is above 10'
+                },
+                {
+                    type: 'setvalue',
+                    expression: '{please_add_a_random_number} = 10',
+                    setToName: 'this_is_the_random_number_you_entered',
+                    setValue: 'It is equal to 10'
+                }];
+
+                newStructure.triggers = [{
+                    type: 'setvalue',
+                    expression: '{please_add_a_random_number} > 10',
+                    setToName: 'this_is_the_random_number_you_entered',
+                    setValue: 'It is above 10'
+                }];
+                */
+
+                if (!_.isEqual(formStructure.triggers, structure.triggers)) { // Accepts undefined and null operands
+
+                    /* ---------------------------------- Search of old triggers ---------------------------------- */
+                    // If both structures contain triggers, store in removedTriggers the triggers that were present in the older structure
+                    if (formStructure.triggers && structure.triggers) {
+                        removedTriggers = formStructure.triggers.filter((prevTrigg) => (
+                            !structure.triggers.some(newTrigg => (_.isEqual(newTrigg, prevTrigg)))
+                        ))
+                    } else { // Else, removedTriggers stores all the older structure triggers
+                        removedTriggers = formStructure.triggers
+                    }
+
+                    // TODO Remove triggers
+
+
+                    /* ---------------------------------- Search of added triggers ---------------------------------- */
+
+
+                    _.union(structure.triggers)
+
+
+                    // TODO Add triggers
+
+
+                    /*
+                    if (removedTriggers && removedTriggers.length) {
+                        structure.triggers = structure.triggers? [...structure.triggers, ...removedTriggers] : removedTriggers; // Add the removed triggers to the new structure
+                    } else {
+                        // No triggers to remove were found
+                    }
+                    console.log('concatened')
+                    console.log(newStructure.triggers)
+                    */
+                }
+
+
+
+                
+                const childForms = await Form.find({ resource: form.resource, _id: { $ne: mongoose.Types.ObjectId(args.id) } }).select('_id structure');
+
+                for (const childForm of childForms) {
+                    let childStructure = JSON.parse(childForm.structure)
+
+                    // TODO Optimize all this, there's way better, with a filter or map or whatever
+                    for (let childTrigger of childStructure.triggers) {
+                        // Remove the old triggers from the children
+                        for (let removedTrigger of removedTriggers) {
+                            if (_.isEqual(childTrigger, removedTrigger)) {
+                                let indexToRemove = childStructure.triggers.findIndex(childTrigger);
+                                childStructure.splice(indexToRemove, 1);
+                            }
+                        }
+                    }
+
+                    // Add the new triggers to the children
+                    childStructure.triggers = childStructure.triggers? _.union(childStructure.triggers, structure.triggers) : structure.triggers;
+
+                    const update = {
+                        structure: childForm.structure,
+                        fields: childForm.fields
+                    };
+                }
+                
+            }
         }
+
+
         // Update version
         const version = new Version({
             createdAt: form.modifiedAt ? form.modifiedAt : form.createdAt,
@@ -204,17 +308,17 @@ export default {
         if (args.status) {
             update.status = args.status;
             if (update.status === status.active) {
-            // Create notification channel
-            const notificationChannel = new Channel({
-                title: `Form - ${form.name}`,
-                form: form._id
-            })
-            await notificationChannel.save();
-            update.channel = notificationChannel.form
+                // Create notification channel
+                const notificationChannel = new Channel({
+                    title: `Form - ${form.name}`,
+                    form: form._id
+                })
+                await notificationChannel.save();
+                update.channel = notificationChannel.form
             } else {
                 // delete channel and notifications if form not active anymore
-                const channel = await Channel.findOneAndDelete({ form: form._id});
-                if (channel)  {
+                const channel = await Channel.findOneAndDelete({ form: form._id });
+                if (channel) {
                     await deleteContent(channel);
                     await Notification.deleteMany({ channel: channel._id });
                     update.channel = [];
