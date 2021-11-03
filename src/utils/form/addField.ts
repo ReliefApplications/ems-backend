280bf92e3675ab388ceb16cc02091a17c6bb9842
Hugo <hugo@reliefapplications.org>
2021-11-03 18:37:00 +0100

    use another structure to add the question #11392

--- src/utils/form/addField.ts
+++ src/utils/form/addField.ts
@@ -4,16 +4,31 @@ import { getQuestion } from './getQuestion';
  * Check if the structure is correct and add a new field at the beginning. Use a passed structure to fetch the correct question.
  * Function by induction.
  * @param structure structure of the form to edit
- * @param name name of the fied to search for
+ * @param name name of the field to search for
  * @param template structure of the core template
  */
 export const addField = (structure: any, name: string, template: any): void => {
-  for (const page in template.pages) {
-    for (const questionIndex in template.pages[page].elements) {
-      if (template.pages[page].elements[questionIndex].name === name) {
-        structure.pages[page].elements.splice(questionIndex, 0, getQuestion(template, name));
-      }
+  let prevQuestion = '';
+  let isFirstQuestion = false;
+  let pageIndex = 0;
+
+  // Find in the template the previous question name
+  template.pages.forEach((page, pIndex) => page.elements.findIndex((el, qIndex) => {
+    if (el.name === name) {
+      isFirstQuestion = qIndex === 0;
+      pageIndex = pIndex;
+      prevQuestion = template.pages[pIndex].elements[`${ isFirstQuestion ? qIndex : qIndex - 1}`].name;
+      return true
     }
-  }
-  
+    return false
+  }));
+
+  // Place the question inside the child form after the previous question if possible
+  structure.pages.forEach((page, pIndex) => page.elements.forEach((el, qIndex) => {
+    if (el.name === prevQuestion && !isFirstQuestion) {
+      structure.pages[pIndex].elements.splice(qIndex + 1, 0, getQuestion(template, name));
+    } else if (isFirstQuestion && pageIndex === pIndex && qIndex === 0) {
+      structure.pages[pIndex].elements.unshift(getQuestion(template, name))
+    }
+  }));
 };
