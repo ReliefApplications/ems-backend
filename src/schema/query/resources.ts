5503758cbf09e1494f4c82dffac2b8e0028c84b0
MartinGbz <martin.grabarz@outlook.fr>
2021-11-15 12:28:39 +0100

    change filters in order to adapt it to the frontend + trying to debug a double spend request to the backend #15952

--- src/schema/query/resources.ts
+++ src/schema/query/resources.ts
@@ -3,8 +3,22 @@ import { ResourceConnectionType, encodeCursor, decodeCursor } from '../types';
 import { Resource } from '../../models';
 import errors from '../../const/errors';
 import { AppAbility } from '../../security/defineAbilityFor';
+import GraphQLJSON from "graphql-type-json";
+import getFilter from "../../utils/filter/getFilter";
 
 const DEFAULT_FIRST = 10;
+
+const FILTER_FIELDS: { name: string, type: string }[] = [
+  {
+    name: 'createdAt',
+    type: 'Date',
+  },
+  {
+    name: 'name',
+    type: 'text',
+  },
+];
+
 
 export default {
   /*  List all resources available for the logged user.
@@ -14,8 +28,18 @@ export default {
   args: {
     first: { type: GraphQLInt },
     afterCursor: { type: GraphQLID },
+    filter: { type: GraphQLJSON },
   },
   async resolve(parent, args, context) {
+    console.log('parent');
+    console.log(parent);
+    console.log('context');
+    console.log(context);
+    console.log('------------------------------------------------------------');
+    console.log(new Date());
+    console.log('------------------------------------------------------------');
+    console.log('args.filter');
+    console.log(args.filter);
     // Authentication check
     const user = context.user;
     if (!user) { throw new GraphQLError(errors.userNotLogged); }
@@ -23,7 +47,11 @@ export default {
     const ability: AppAbility = context.user.ability;
 
     const abilityFilters = Resource.accessibleBy(ability, 'read').getFilter();
-    const filters: any[] = [abilityFilters];
+    const queryFilters = getFilter(args.filter, FILTER_FIELDS);
+    console.log('queryFilters');
+    console.log(queryFilters);
+
+    const filters: any[] = [queryFilters, abilityFilters];
 
     const first = args.first || DEFAULT_FIRST;
     const afterCursor = args.afterCursor;
@@ -35,6 +63,11 @@ export default {
 
     let items: any[] = await Resource.find({ $and: [cursorFilters, ...filters] })
       .limit(first + 1);
+
+    console.log('items');
+    console.log(items.map(x => x.name));
+    console.log('(queryFilters)');
+    console.log(queryFilters);
 
     const hasNextPage = items.length > first;
     if (hasNextPage) {
