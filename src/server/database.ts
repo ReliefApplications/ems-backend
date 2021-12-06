83ac7e45d96597b0587c588642afab13eec74849
Pacôme Rivier <pacome@reliefapplications.org>
2021-12-06 12:46:33 +0100

    feat: update CI to deploy mongodb test database #16673

--- src/server/database.ts
+++ src/server/database.ts
@@ -3,14 +3,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const mongoDBUrl = (): string => {
+  if (process.env.CI) {
+    return `${process.env.DB_PREFIX}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  }
   if (process.env.COSMOS_DB_PREFIX) {
     return `${process.env.COSMOS_DB_PREFIX}://${process.env.COSMOS_DB_USER}:${process.env.COSMOS_DB_PASS}@${process.env.COSMOS_DB_HOST}:${process.env.COSMOS_DB_PORT}/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@${process.env.COSMOS_APP_NAME}@`;
-  } else {
-    if (process.env.DB_PREFIX === 'mongodb+srv') {
-      return `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-    } else {
-      return `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${process.env.APP_NAME}@`;
-    }
+  }
+  if (process.env.DB_PREFIX === 'mongodb+srv') {
+    return `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  } else {  
+    return `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@${process.env.APP_NAME}@`;
   }
 };
 
