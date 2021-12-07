92ddc830a4b65c5aed70abd235f686714476eef4
Pacôme Rivier <pacome@reliefapplications.org>
2021-12-07 17:25:37 +0100

    fix: adapt integration tests to be more precise #16673

--- __tests__/integration.test.ts
+++ __tests__/integration.test.ts
@@ -1,6 +1,6 @@
 import errors from '../src/const/errors';
 import { token, request } from './jest.setup';
-import { Client, Role } from '../src/models';
+import { Client, Role, Application } from '../src/models';
 
 describe('End-to-end tests', () => {
   test('query that does not exist returns 400', async () => {
@@ -14,8 +14,8 @@ describe('End-to-end tests', () => {
     expect(response.status).toBe(400);
   });
 
-  const query = '{ applications { edges { node { id } } } }';
   test('query without auth token returns error', async () => {
+    const query = '{ applications { edges { node { id } } } }';
     const response = await request
       .post('/graphql')
       .send({ query })
@@ -32,27 +32,62 @@ describe('End-to-end tests', () => {
   });
 
   test('query with auth token and without roles returns empty', async () => {
+    const appName = 'Automated test';
+    await Application.findOneAndDelete({ name: appName });
+    const application = await (new Application({
+      name: appName,
+    })).save();
+    const query = 'query getApplications($id: ID!) {\
+      application(id: $id) { name, id }\
+    }';
+    const variables = {
+      id: application._id,
+    };
     await Client.findOneAndUpdate({ clientId: process.env.clientID }, { roles: [] });
     const response = await request
       .post('/graphql')
-      .send({ query })
+      .send({ query, variables })
       .set('Authorization', token)
       .set('Accept', 'application/json');
     expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty(['data', 'applications', 'edges']);
-    expect(response.body.data.applications.edges).toEqual([]);
+    expect(response.body).toHaveProperty('errors');
+    expect(response.body.errors).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: errors.permissionNotGranted,
+        }),
+      ]),
+    );
+    await Application.findOneAndDelete({ name: appName });
   });
 
   test('query with auth token and admin role returns success', async () => {
+    const appName = 'Automated test';
+    await Application.findOneAndDelete({ name: appName });
+    const application = await (new Application({
+      name: appName,
+    })).save();
+    const query = 'query getApplications($id: ID!) {\
+      application(id: $id) { name, id }\
+    }';
+    const variables = {
+      id: application._id,
+    };
     const admin = await Role.findOne({ title: 'admin' });
     await Client.findOneAndUpdate({ clientId: process.env.clientID }, { roles: [ admin._id ] });
     const response = await request
       .post('/graphql')
-      .send({ query })
+      .send({ query, variables })
       .set('Authorization', token)
       .set('Accept', 'application/json');
     expect(response.status).toBe(200);
-    expect(Boolean(response.body.errors)).toBe(false);
-    expect(response.body).toHaveProperty(['data', 'applications', 'edges']);
+    expect(response.body).not.toHaveProperty('errors');
+    expect(response.body).toHaveProperty(['data', 'application']);
+    expect(response.body.data.application).toEqual(
+      expect.objectContaining({
+        id: String(application._id),
+        name: application.name,
+      }));
+    await Application.findOneAndDelete({ name: appName });
   });
 });
