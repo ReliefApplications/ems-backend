9aac75ad063b45d4d005e59959f4bd349928fba3
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-01-13 11:54:47 +0100

    add test for me query #16943

---
+++ __tests__/schema/query/me.test.ts
@@ -0,0 +1,27 @@
+import { ApolloServer } from 'apollo-server-express';
+import schema from '../../../src/schema';
+import { SafeTestServer } from '../../server.setup';
+import { User } from '../../../src/models';
+
+let server: ApolloServer;
+
+describe('ME query tests', () => {
+  const query = '{ me: { id username } }';
+
+  test('query with no token returns error', async () => {
+    server = await SafeTestServer.createApolloTestServer(schema, null);
+    const result = await server.executeOperation({ query });
+    expect(result.errors).toBeDefined();
+  });
+
+  test('query with token should return user info', async () => {
+    const dummyUser = new User({ username: 'dummy' });
+    await dummyUser.save();
+    server = await SafeTestServer.createApolloTestServer(schema, dummyUser);
+    const result = await server.executeOperation({ query });
+    expect(result.errors).toBeUndefined();
+    expect(result).toHaveProperty(['data']);
+    expect(result.data?.id).toEqual(dummyUser.id);
+    expect(result.data?.username).toEqual(dummyUser.username);
+  });
+});
