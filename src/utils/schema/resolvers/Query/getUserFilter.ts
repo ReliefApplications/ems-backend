46cbb5e985c9c05b74eac13bd64796ec2434ab57
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-03-11 12:49:50 +0100

    update lint

--- src/utils/schema/resolvers/Query/getUserFilter.ts
+++ src/utils/schema/resolvers/Query/getUserFilter.ts
@@ -4,12 +4,21 @@ const USER_DEFAULT_FIELDS = ['createdBy', 'lastUpdatedBy'];
 
 /**
  * Transforms query filter into user mongo filter.
+ *
  * @param filter filter to transform to user mongo filter.
+ * @param fields list of fields
+ * @param context context of request
  * @returns User mongo filter.
  */
-const buildUserMongoFilter = (filter: any, fields: any[], context: any): any => {
+const buildUserMongoFilter = (
+  filter: any,
+  fields: any[],
+  context: any
+): any => {
   if (filter.filters) {
-    const filters = filter.filters.map((x: any) => buildUserMongoFilter(x, fields, context)).filter(x => x);
+    const filters = filter.filters
+      .map((x: any) => buildUserMongoFilter(x, fields, context))
+      .filter((x) => x);
     if (filters.length > 0) {
       switch (filter.logic) {
         case 'and': {
@@ -27,12 +36,12 @@ const buildUserMongoFilter = (filter: any, fields: any[], context: any): any => 
     }
   } else {
     if (filter.field) {
-
       if (filter.field === 'ids') {
-        return { _id: { $in: filter.value.map(x => mongoose.Types.ObjectId(x)) } };
+        return {
+          _id: { $in: filter.value.map((x) => mongoose.Types.ObjectId(x)) },
+        };
       }
       if (filter.operator) {
-
         const [field, subField] = filter.field.split('.');
 
         if (!USER_DEFAULT_FIELDS.includes(field)) {
@@ -45,25 +54,40 @@ const buildUserMongoFilter = (filter: any, fields: any[], context: any): any => 
 
         try {
           intValue = Number(value);
-        } catch { }
+        } catch {}
 
         switch (filter.operator) {
           case 'eq': {
             if (isNaN(intValue)) {
               return { [fieldName]: { $eq: value } };
             } else {
-              return { $or: [{ [fieldName]: { $eq: value } }, { [fieldName]: { $eq: intValue } }] };
+              return {
+                $or: [
+                  { [fieldName]: { $eq: value } },
+                  { [fieldName]: { $eq: intValue } },
+                ],
+              };
             }
           }
           case 'neq': {
             if (isNaN(intValue)) {
               return { [fieldName]: { $ne: value } };
             } else {
-              return { $or: [{ [fieldName]: { $ne: value } }, { [fieldName]: { $ne: intValue } }] };
+              return {
+                $or: [
+                  { [fieldName]: { $ne: value } },
+                  { [fieldName]: { $ne: intValue } },
+                ],
+              };
             }
           }
           case 'isnull': {
-            return { $or: [{ [fieldName]: { $exists: false } }, { [fieldName]: { $eq: null } }] };
+            return {
+              $or: [
+                { [fieldName]: { $exists: false } },
+                { [fieldName]: { $eq: null } },
+              ],
+            };
           }
           case 'isnotnull': {
             return { [fieldName]: { $exists: true, $ne: null } };
@@ -72,28 +96,48 @@ const buildUserMongoFilter = (filter: any, fields: any[], context: any): any => 
             if (isNaN(intValue)) {
               return { [fieldName]: { $lt: value } };
             } else {
-              return { $or: [{ [fieldName]: { $lt: value } }, { [fieldName]: { $lt: intValue } }] };
+              return {
+                $or: [
+                  { [fieldName]: { $lt: value } },
+                  { [fieldName]: { $lt: intValue } },
+                ],
+              };
             }
           }
           case 'lte': {
             if (isNaN(intValue)) {
               return { [fieldName]: { $lte: value } };
             } else {
-              return { $or: [{ [fieldName]: { $lte: value } }, { [fieldName]: { $lte: intValue } }] };
+              return {
+                $or: [
+                  { [fieldName]: { $lte: value } },
+                  { [fieldName]: { $lte: intValue } },
+                ],
+              };
             }
           }
           case 'gt': {
             if (isNaN(intValue)) {
               return { [fieldName]: { $gt: value } };
             } else {
-              return { $or: [{ [fieldName]: { $gt: value } }, { [fieldName]: { $gt: intValue } }] };
+              return {
+                $or: [
+                  { [fieldName]: { $gt: value } },
+                  { [fieldName]: { $gt: intValue } },
+                ],
+              };
             }
           }
           case 'gte': {
             if (isNaN(intValue)) {
               return { [fieldName]: { $gte: value } };
             } else {
-              return { $or: [{ [fieldName]: { $gte: value } }, { [fieldName]: { $gte: intValue } }] };
+              return {
+                $or: [
+                  { [fieldName]: { $gte: value } },
+                  { [fieldName]: { $gte: intValue } },
+                ],
+              };
             }
           }
           case 'startswith': {
