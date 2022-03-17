fc9b85b76114d985c6c785a1c292c50cee43d5b4
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-03-17 15:11:45 +0100

    feat: email from back #25833

---
+++ src/routes/email/index.ts
@@ -0,0 +1,164 @@
+import express from 'express';
+import * as nodemailer from 'nodemailer';
+import * as dotenv from 'dotenv';
+import { extractGridData } from '../../utils/files';
+import { preprocess } from '../../utils/email';
+import xlsBuilder from '../../utils/files/xlsBuilder';
+import { EmailPlaceholder } from '../../const/email';
+dotenv.config();
+
+/**
+ * Transforms a list into a flat list.
+ *
+ * @param arr list.
+ * @returns flat list.
+ */
+const flatDeep = (arr: any[]): any[] => {
+  return arr.reduce(
+    (acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val) : val),
+    []
+  );
+};
+
+/**
+ * Gets flat fields from list of query fields
+ *
+ * @param fields query fields
+ * @param prefix fields prefix
+ * @returns flat fields
+ */
+const getFields = (fields: any[], prefix?: string): any[] => {
+  return flatDeep(
+    fields
+      .filter((x) => x.kind !== 'LIST')
+      .map((f) => {
+        switch (f.kind) {
+          case 'OBJECT': {
+            return getFields(f.fields, f.name);
+          }
+          default: {
+            const path = prefix ? `${prefix}.${f.name}` : f.name;
+            return { name: path, title: f.label ? f.label : f.name };
+          }
+        }
+      }),
+  );
+};
+
+/**
+ * Send email using SMTP email client
+ */
+const router = express.Router();
+
+/**
+ * Send email using SMTP email client
+ *
+ * @param recipient Recipient of the email.
+ * @param subject Subject of the email.
+ * @param body Body of the email, if not given we put the formatted records.
+ * @param gridSettings Grid specific settings.
+ * @param gridSettings.query Query settings.
+ * @param gridSettings.query.name Name of the query.
+ * @param gridSettings.query.fields Fields of the query.
+ * @param gridSettings.ids List of records to include in the email.
+ * @param gridSettings.sortField Sort field.
+ * @param gridSettings.sortOrder Sort order.
+ * @param attachment Whether an excel with the dataset is attached to the mail or not.
+ */
+router.post('/', async (req, res) => {
+  // Authentication check
+  const user = req.context.user;
+  if (!user) {
+    return res.status(401).send('User not connected');
+  }
+  // Retrieving parameters
+  const args = req.body;
+  if (!args.recipient || (!args.subject && !args.body)) {
+    return res.status(400).send('Missing parameters');
+  }
+
+  // Create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    requireTLS: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
+
+  // Fetch records data for attachment / body if needed
+  const attachments: any[] = [];
+  let fileName: string;
+  let columns: any[];
+  let rows: any[];
+  // Query data if attachment or dataset in email body
+  if (args.attachment || args.body.includes(EmailPlaceholder.DATASET)) {
+    ({ columns, rows } = await extractGridData(
+      {
+        query: args.gridSettings.query,
+        ids: args.gridSettings.ids,
+        sortField: args.gridSettings.sortField,
+        sortOrder: args.gridSettings.sortOrder,
+        format: 'xlsx',
+        fields: getFields(args.gridSettings.query.fields),
+        filter: {
+          logic: 'and',
+          filters: [
+            {
+              operator: 'eq',
+              field: 'ids',
+              value: args.gridSettings.ids,
+            },
+          ],
+        },
+      },
+      req.headers.authorization,
+    ));
+  }
+  // Attach excel
+  if (args.attachment) {
+    const today = new Date();
+    const month = today.toLocaleString('en-us', { month: 'short' });
+    const date = month + ' ' + today.getDate() + ' ' + today.getFullYear();
+    const name = args.gridSettings.query.name.substring(3);
+    fileName = name + ' ' + date;
+    const file = await xlsBuilder(fileName, columns, rows);
+    attachments.push({
+      filename: `${fileName}.xlsx`,
+      content: file,
+    });
+  }
+
+  // Preprocess body and subject
+  const body = preprocess(args.body, {
+    fields: columns,
+    rows,
+  });
+  const subject = preprocess(args.subject);
+
+  // Send mail
+  try {
+    const info = await transporter.sendMail({
+      from: `"No reply" <${process.env.MAIL_USER}>`,
+      to: args.recipient,
+      subject,
+      html: body,
+      attachments,
+    });
+    if (info.messageId) {
+      return res.status(200).send({ status: 'OK' });
+    } else {
+      return res
+        .status(400)
+        .send({ status: 'SMTP server failed to send the email' });
+    }
+  } catch {
+    return res
+      .status(400)
+      .send({ status: 'SMTP server failed to send the email' });
+  }
+});
+
+export default router;
--- src/utils/files/csvBuilder.ts
+++ src/utils/files/csvBuilder.ts
@@ -1,10 +1,21 @@
 import { Parser } from 'json2csv';
 import get from 'lodash/get';
 
-export default (res, fileName: string, columns: any[], data) => {
-  const columnsNames = columns.map(x => x.name);
-  const json2csv = new Parser({ columnsNames });
+/**
+ * Builds a CSV file.
+ *
+ * @param columns Array of objects with a name property that will match the data, and optionally a label that will be the column title on the exported file
+ * @param data Array of objects, that will be transformed into the rows of the csv. Each object should have [key, value] as [column's name, corresponding value].
+ * @returns response with file attached.
+ */
+export default (columns: any[], data) => {
+  // Create a string array with the columns' labels or names as fallback, then construct the parser from it
+  const columnsNames = columns.flatMap((x) => (x.label ? x.label : x.name));
+  const json2csv = new Parser(columnsNames);
+
   const tempCsv = [];
+
+  // Build an object for each row, and push it in an array
   for (const row of data) {
     const temp = {};
     for (const field of columns) {
@@ -12,8 +23,8 @@ export default (res, fileName: string, columns: any[], data) => {
     }
     tempCsv.push(temp);
   }
+
+  // Generate the file by parsing the data, set the response parameters and send it
   const csv = json2csv.parse(tempCsv);
-  res.header('Content-Type', 'text/csv');
-  res.attachment(fileName);
-  return res.send(csv);
+  return csv;
 };
