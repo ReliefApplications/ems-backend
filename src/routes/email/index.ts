62c2e147002c9cfdc6095c810e766313c168210b
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-03-18 10:00:58 +0100

    feat: preview email #27626

--- src/routes/email/index.ts
+++ src/routes/email/index.ts
@@ -7,6 +7,8 @@ import xlsBuilder from '../../utils/files/xlsBuilder';
 import { EmailPlaceholder } from '../../const/email';
 dotenv.config();
 
+const EMAIL_FROM = `"No reply" <${process.env.MAIL_USER}>`;
+
 /**
  * Transforms a list into a flat list.
  *
@@ -16,7 +18,7 @@ dotenv.config();
 const flatDeep = (arr: any[]): any[] => {
   return arr.reduce(
     (acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val) : val),
-    []
+    [],
   );
 };
 
@@ -45,49 +47,12 @@ const getFields = (fields: any[], prefix?: string): any[] => {
   );
 };
 
-/**
- * Send email using SMTP email client
- */
-const router = express.Router();
-
-/**
- * Send email using SMTP email client
- *
- * @param recipient Recipient of the email.
- * @param subject Subject of the email.
- * @param body Body of the email, if not given we put the formatted records.
- * @param gridSettings Grid specific settings.
- * @param gridSettings.query Query settings.
- * @param gridSettings.query.name Name of the query.
- * @param gridSettings.query.fields Fields of the query.
- * @param gridSettings.ids List of records to include in the email.
- * @param gridSettings.sortField Sort field.
- * @param gridSettings.sortOrder Sort order.
- * @param attachment Whether an excel with the dataset is attached to the mail or not.
- */
-router.post('/', async (req, res) => {
-  // Authentication check
-  const user = req.context.user;
-  if (!user) {
-    return res.status(401).send('User not connected');
-  }
+const generateEmail = async (req, res) => {
   // Retrieving parameters
   const args = req.body;
   if (!args.recipient || (!args.subject && !args.body)) {
     return res.status(400).send('Missing parameters');
   }
-
-  // Create reusable transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    requireTLS: true,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS,
-    },
-  });
-
   // Fetch records data for attachment / body if needed
   const attachments: any[] = [];
   let fileName: string;
@@ -137,15 +102,62 @@ router.post('/', async (req, res) => {
     rows,
   });
   const subject = preprocess(args.subject);
+  return {
+    recipient: args.recipient,
+    subject,
+    body,
+    attachments,
+  };
+};
+
+/**
+ * Send email using SMTP email client
+ */
+const router = express.Router();
+
+/**
+ * Send email using SMTP email client
+ *
+ * @param recipient Recipient of the email.
+ * @param subject Subject of the email.
+ * @param body Body of the email, if not given we put the formatted records.
+ * @param gridSettings Grid specific settings.
+ * @param gridSettings.query Query settings.
+ * @param gridSettings.query.name Name of the query.
+ * @param gridSettings.query.fields Fields of the query.
+ * @param gridSettings.ids List of records to include in the email.
+ * @param gridSettings.sortField Sort field.
+ * @param gridSettings.sortOrder Sort order.
+ * @param attachment Whether an excel with the dataset is attached to the mail or not.
+ */
+router.post('/', async (req, res) => {
+  // Authentication check
+  const user = req.context.user;
+  if (!user) {
+    return res.status(401).send('User not connected');
+  }
+
+  const email = await generateEmail(req, res);
+
+  // Create reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    requireTLS: true,
+    auth: {
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS,
+    },
+  });
 
   // Send mail
   try {
     const info = await transporter.sendMail({
-      from: `"No reply" <${process.env.MAIL_USER}>`,
-      to: args.recipient,
-      subject,
-      html: body,
-      attachments,
+      from: EMAIL_FROM,
+      to: email.recipient,
+      subject: email.subject,
+      html: email.body,
+      attachments: email.attachments,
     });
     if (info.messageId) {
       return res.status(200).send({ status: 'OK' });
@@ -159,6 +171,23 @@ router.post('/', async (req, res) => {
       .status(400)
       .send({ status: 'SMTP server failed to send the email' });
   }
+});
+
+router.post('/preview', async (req, res) => {
+  // Authentication check
+  const user = req.context.user;
+  if (!user) {
+    return res.status(401).send('User not connected');
+  }
+
+  const email = await generateEmail(req, res);
+  return res.json({
+    from: EMAIL_FROM,
+    to: email.recipient,
+    subject: email.subject,
+    html: email.body,
+    // attachments: email.attachments,
+  });
 });
 
 export default router;
