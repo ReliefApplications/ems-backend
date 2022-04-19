1960a2af915211c339c0a28ce0c52afd87e3460d
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-04-19 15:12:38 +0200

    more logs

--- src/utils/proxy/buildProxies.ts
+++ src/utils/proxy/buildProxies.ts
@@ -19,6 +19,7 @@ export const buildProxies = async (app): Promise<void> => {
     // Add a middleware to fetch the auth token, only working workaround from this thread:
     // https://github.com/chimurai/http-proxy-middleware/issues/318#issuecomment-582098177
     app.use(safeEndpoint, (req, res, next) => {
+      console.log(apiConfiguration.name);
       getToken(apiConfiguration).then(token => {
         req.__token = token;
         next();
@@ -33,17 +34,20 @@ export const buildProxies = async (app): Promise<void> => {
 
     // Redirect safe endpoint using proxy
     app.use(safeEndpoint, (req, res) => {
+      console.log(apiConfiguration.name + 2);
       proxy.web(req, res, { target: apiConfiguration.endpoint });
     });
 
     // On proxy request, attach headers with auth token
     proxy.on('proxyReq', (proxyReq: ClientRequest, req) => {
       // Attach auth token
+      console.log(apiConfiguration.name + 3);
       proxyReq.setHeader('Authorization', 'Bearer ' + req.__token);
     });
 
     // Prevent crashing if request is aborted by client before being fullfilled
     proxy.on('error', (err, req, res) => {
+      console.log(apiConfiguration.name + 4);
       req.destroy();
       res.writeHead(500, {
         'Content-Type': 'text/plain',
