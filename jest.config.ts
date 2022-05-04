7886c649f1a4c0c8b3297c6b9afd6ae6d98e582a
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-05-04 13:08:40 +0200

    remove coverage of __tests__

--- jest.config.ts
+++ jest.config.ts
@@ -26,9 +26,10 @@ export default {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/__tests__/"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
