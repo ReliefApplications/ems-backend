d9a6e684284a61195bcb06fd1644d7cf7336a3bb
Pacôme Rivier <pacome@reliefapplications.org>
2022-06-14 18:09:18 +0200

    feat: assign correct roles to public client

--- src/server/middlewares/rest.ts
+++ src/server/middlewares/rest.ts
@@ -1,5 +1,6 @@
 import passport from 'passport';
 import defineAbilitiesFor from '../../security/defineAbilityFor';
+import defineUser from '../../security/defineUser';
 import { authenticationType } from '../../oort.config';
 import * as dotenv from 'dotenv';
 import i18next from 'i18next';
@@ -19,9 +20,9 @@ const strategy =
  * @param next Callback argument to the middleware function
  */
 export const restMiddleware = (req, res, next) => {
-  passport.authenticate(strategy, { session: false }, (err, user) => {
+  passport.authenticate(strategy, { session: false }, async (err, user) => {
     if (user) {
-      req.context = { user };
+      req.context = { user: await defineUser(user, req) };
       // req.context.user = user;
       // Define the rights of the user
       req.context.user.ability = defineAbilitiesFor(user);
