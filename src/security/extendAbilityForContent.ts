d0977c45ce101638d7f853bb61e671ef27efea69
Alexis Delage <alexisd.reliefapps@gmail.com>
2022-08-11 09:04:57 +0200

    Add ability for steps #38858

---
+++ src/security/extendAbilityForContent.ts
@@ -0,0 +1,104 @@
+import { AbilityBuilder, Ability, AbilityClass } from '@casl/ability';
+import { clone } from 'lodash';
+import { AppAbility, conditionsMatcher, Actions } from './defineUserAbility';
+import {
+  Application,
+  Dashboard,
+  Form,
+  Page,
+  Step,
+  User,
+  Workflow,
+} from '../models';
+import { extendAbilityForPageOnPage } from './extendAbilityForPage';
+import { extendAbilityForStepOnStep } from './extendAbilityForStep';
+
+/** Application ability class */
+const appAbility = Ability as AbilityClass<AppAbility>;
+
+export default async function extendAbilityForContent(
+  user: User,
+  content: Form,
+  container: Page | Step,
+  application?: Application,
+  ability?: AppAbility
+): Promise<AppAbility>;
+export default async function extendAbilityForContent(
+  user: User,
+  content: Dashboard | Workflow,
+  container?: Page | Step,
+  application?: Application,
+  ability?: AppAbility
+): Promise<AppAbility>;
+
+/**
+ * Extends the user abilities for a page or step content, ie a workflow,
+ * a dashboard, or a form.
+ * For a form, you must indicate on which page or step it is used.
+ *
+ * @param user The user instance
+ * @param content The content object (a workflow, dashboard, or form)
+ * @param container The page or step which contains the content (optional for workflows and dashboards)
+ * @param application The application of this content, if known, to reduce calculations (optional)
+ * @param ability The ability object to extend from, if different form the user ability (optional)
+ * @returns ability definition of the user
+ */
+export default async function extendAbilityForContent(
+  user: User,
+  content: Workflow | Dashboard | Form,
+  container?: Page | Step,
+  application?: Application,
+  ability?: AppAbility
+): Promise<AppAbility> {
+  if (ability === undefined) ability = user.ability;
+  if (container === undefined) {
+    container = await Page.findOne({ content: content._id });
+    if (!container) container = await Step.findOne({ content: content._id });
+  }
+  if (application === undefined) {
+    if (container instanceof Page) {
+      application = await Application.findOne({ pages: container._id });
+    } else {
+      const workflow = await Workflow.findOne({ steps: container._id });
+      const page = await Page.findOne({ content: workflow._id });
+      application = await Application.findOne({ pages: page._id });
+    }
+  }
+
+  // get the permissions of the page or the step
+  if (container instanceof Page) {
+    ability = await extendAbilityForPageOnPage(
+      user,
+      container,
+      application,
+      ability
+    );
+  } else {
+    ability = await extendAbilityForStepOnStep(
+      user,
+      container,
+      application,
+      ability
+    );
+  }
+
+  const abilityBuilder = new AbilityBuilder(appAbility);
+  const can = abilityBuilder.can;
+
+  // copy the existing global abilities from the user
+  abilityBuilder.rules = clone(ability.rules);
+
+  // for each permission, give it if one page with this content already has the permission
+  for (const action of ['read', 'update', 'delete'] as Actions[]) {
+    if (ability.cannot(action, content) && ability.can(action, container)) {
+      if (content instanceof Dashboard)
+        can(action, 'Dashboard', { _id: content._id });
+      if (content instanceof Workflow)
+        can(action, 'Workflow', { _id: content._id });
+      if (content instanceof Form) can(action, 'Form', { _id: content._id });
+    }
+  }
+
+  // return the new ability instance
+  return abilityBuilder.build({ conditionsMatcher });
+}
