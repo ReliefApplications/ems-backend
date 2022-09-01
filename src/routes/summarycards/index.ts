7989cd3d7664c7ea3eefc1bf438da7f1a795c0e8
LenaelleRelief <lenaelle.reliefapps@gmail.com>
2022-09-01 17:18:29 +0200

    remove filtering only the 3 most recent #34844

--- src/routes/summarycards/index.ts
+++ src/routes/summarycards/index.ts
@@ -44,7 +44,7 @@ router.get('/templates', async (req, res) => {
     listOfSummaryCards.push(json);
   });
 
-  res.send(listOfSummaryCards.flat().slice(-3).reverse());
+  res.send(listOfSummaryCards.flat());
 });
 
 export default router;
