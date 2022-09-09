f355fd414decece69f7a1713af499bb4d85cdcb9
Pacôme Rivier <pacome@reliefapplications.org>
2022-09-09 14:10:47 +0200

    change email placeholders

--- src/const/email.ts
+++ src/const/email.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 /** Enum definition for placeholder text in emails */
 export enum EmailPlaceholder {
-  TODAY = '{today}',
-  DATASET = '{dataset}',
-  NOW = '{now}',
+  TODAY = '{{today}}',
+  DATASET = '{{dataset}}',
+  NOW = '{{now}}',
 }
