1b73bf770b4612dad9bda11e1d3997602aaa914e
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-09-15 12:13:57 +0530

    OORT-441 Fix style maintainability security issues raised by CodeFactor

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo -e "Stopping docker containers..."
-CMD="cd `$REMOTE_PATH` && echo ${$SSH_PASS} | sudo -S docker-compose pull api"
+CMD="cd `${$REMOTE_PATH}` && echo `${$SSH_PASS}` | sudo -S docker-compose pull api"
 ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "'"$CMD"'"
 
 echo -e "Stopping docker containers..."
