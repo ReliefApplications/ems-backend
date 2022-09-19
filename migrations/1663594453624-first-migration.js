de02e124b0134a98b857e510706ba5203aef2233
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-09-19 20:32:15 +0530

    OORT-443 add migration file

--- migrations/1663594453624-first-migration.js
+++ migrations/1663594453624-first-migration.js
@@ -2,7 +2,6 @@ const { startDatabase } = require('../src/server/database.ts')
 
 module.exports.up = async function (next) {
   
-  //console.log("conn ===>> ", db);
   console.log(" =========>>>>>> UP <<<<<<<============ ");
   //next()
 }
