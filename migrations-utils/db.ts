5190818c323d400716146ac448224d9040d86445
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-09-20 19:28:00 +0530

    OORT-443 add aggregations migration

--- migrations-utils/db.ts
+++ migrations-utils/db.ts
@@ -1,15 +1,15 @@
 import mongoose from 'mongoose';
-const env = require("dotenv").config();
+const env = require('dotenv').config();
 
 export const getDb = async () => {
-    const mongoUrl = `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-    await mongoose.connect(mongoUrl, {
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        autoIndex: true,
-        autoReconnect: true,
-        reconnectInterval: 5000,
-        reconnectTries: 3,
-        poolSize: 10,
-    });
+  const mongoUrl = `${process.env.DB_PREFIX}://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+  await mongoose.connect(mongoUrl, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    autoIndex: true,
+    autoReconnect: true,
+    reconnectInterval: 5000,
+    reconnectTries: 3,
+    poolSize: 10,
+  });
 };
