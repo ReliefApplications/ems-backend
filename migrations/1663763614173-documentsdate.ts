ee27f62a270b9ce3b394d366f9910537521ff45b
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-09-22 17:28:18 +0530

    OORT-422 creating diff query for createdAt and modifiedAt column

--- migrations/1663763614173-documentsdate.ts
+++ migrations/1663763614173-documentsdate.ts
@@ -18,332 +18,172 @@ import {
 startDatabaseForMigration();
 
 /**
+ * get query for createdAt column add.
+ *
+ * @return query return of createdAt
+ */
+const getCreatedAtQuery = async () => {
+  return [
+    {
+      $set: {
+        createdAt: {
+          $convert: {
+            input: '$_id',
+            to: 'date',
+          },
+        },
+      },
+    },
+  ];
+};
+
+/**
+ * get query for modifiedAt column add.
+ *
+ * @return query return of modifiedAt
+ */
+const getModifiedAtQuery = async () => {
+  return [
+    {
+      $set: {
+        modifiedAt: {
+          $convert: {
+            input: '$_id',
+            to: 'date',
+          },
+        },
+      },
+    },
+  ];
+};
+
+/**
  * Use to documents date migrate up.
  *
  * @returns just migrate data.
  */
 export const up = async () => {
-  //add createdAt and modifiedAt column in the application model
+  const createdAtQuery = await getCreatedAtQuery();
+  const modifiedAtQuery = await getModifiedAtQuery();
+
+  //add createdAt column in the application model
   await Application.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
-
-  //add createdAt and modifiedAt column in the dashboard model
-  await Dashboard.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
-
-  //add createdAt and modifiedAt column in the form model
-  await Form.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
-
-  //add createdAt and modifiedAt column in the group model
-  await Group.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
-
-  //add createdAt and modifiedAt column in the page model
-  await Page.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
-
-  //add createdAt and modifiedAt column in the record model
-  await Record.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
-
-  //add createdAt and modifiedAt column in the resource model
-  await Resource.updateMany(
     { createdAt: { $exists: false } },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
+    createdAtQuery,
     { timestamps: false }
   );
 
-  //add createdAt and modifiedAt column in the referenceData model
+  //add modifiedAt column in the application model
+  await Application.updateMany(
+    { modifiedAt: { $exists: false } },
+    modifiedAtQuery,
+    { timestamps: false }
+  );
+
+  //add createdAt column in the dashboard model
+  await Dashboard.updateMany(
+    { createdAt: { $exists: false } },
+    createdAtQuery,
+    { timestamps: false }
+  );
+
+  //add modifiedAt column in the dashboard model
+  await Dashboard.updateMany(
+    { modifiedAt: { $exists: false } },
+    modifiedAtQuery,
+    { timestamps: false }
+  );
+
+  //add createdAt column in the form model
+  await Form.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
+
+  //add modifiedAt column in the form model
+  await Form.updateMany({ modifiedAt: { $exists: false } }, modifiedAtQuery, {
+    timestamps: false,
+  });
+
+  //add createdAt column in the group model
+  await Group.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
+
+  //add modifiedAt column in the group model
+  await Group.updateMany({ modifiedAt: { $exists: false } }, modifiedAtQuery, {
+    timestamps: false,
+  });
+
+  //add createdAt column in the page model
+  await Page.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
+
+  //add modifiedAt column in the page model
+  await Page.updateMany({ modifiedAt: { $exists: false } }, modifiedAtQuery, {
+    timestamps: false,
+  });
+
+  //add createdAt column in the record model
+  await Record.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
+
+  //add modifiedAt column in the record model
+  await Record.updateMany({ modifiedAt: { $exists: false } }, modifiedAtQuery, {
+    timestamps: false,
+  });
+
+  //add createdAt column in the resource model
+  await Resource.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
+
+  //add modifiedAt column in the referenceData model
   await ReferenceData.updateMany(
     { modifiedAt: { $exists: false } },
-    [
-      {
-        $set: {
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
+    modifiedAtQuery,
     { timestamps: false }
   );
 
-  //add createdAt and modifiedAt column in the setting model
+  //add modifiedAt column in the setting model
   await Setting.updateMany(
     { modifiedAt: { $exists: false } },
-    [
-      {
-        $set: {
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
+    modifiedAtQuery,
     { timestamps: false }
   );
 
-  //add createdAt and modifiedAt column in the step model
-  await Step.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
+  //add createdAt column in the step model
+  await Step.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
 
-  //add createdAt and modifiedAt column in the user model
-  await User.updateMany(
-    { modifiedAt: { $exists: false } },
-    [
-      {
-        $set: {
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
+  //add modifiedAt column in the step model
+  await Step.updateMany({ modifiedAt: { $exists: false } }, modifiedAtQuery, {
+    timestamps: false,
+  });
 
-  //add createdAt and modifiedAt column in the Workflow model
+  //add modifiedAt column in the user model
+  await User.updateMany({ modifiedAt: { $exists: false } }, modifiedAtQuery, {
+    timestamps: false,
+  });
+
+  //add createdAt column in the Workflow model
+  await Workflow.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
+
+  //add modifiedAt column in the Workflow model
   await Workflow.updateMany(
-    {
-      $or: [
-        { createdAt: { $exists: false } },
-        { modifiedAt: { $exists: false } },
-      ],
-    },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-          modifiedAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
+    { modifiedAt: { $exists: false } },
+    modifiedAtQuery,
     { timestamps: false }
   );
 
-  //add createdAt and modifiedAt column in the Version model
-  await Version.updateMany(
-    { createdAt: { $exists: false } },
-    [
-      {
-        $set: {
-          createdAt: {
-            $convert: {
-              input: '$_id',
-              to: 'date',
-            },
-          },
-        },
-      },
-    ],
-    { timestamps: false }
-  );
+  //add createdAt column in the Version model
+  await Version.updateMany({ createdAt: { $exists: false } }, createdAtQuery, {
+    timestamps: false,
+  });
 };
 
 /**
