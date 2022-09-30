d7cd43731ce1b5a83dd1a6124a7bfd47dcade5fd
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-09-30 17:18:53 +0530

    OORT-465 Check CI tests

---
+++ config/test.js
@@ -0,0 +1,15 @@
+/**
+ * Configuration of back-office
+ * Use https://www.npmjs.com/package/config package.
+ */
+ module.exports = {
+    database: {
+      provider: '',
+      prefix: '',
+      host: '',
+      port: '',
+      name: '',
+      user: '',
+      pass: '',
+    }
+  };
