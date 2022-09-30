48e1417f6bc83b0bb9cd8a495440168fd5717cd7
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-09-30 19:40:53 +0530

    OORT-465 Check CI tests revert

--- config/test.js
+++ config/test.js
@@ -5,12 +5,12 @@
 
  module.exports = {
     database: {
-      provider: 'docker',
-      prefix: 'mongodb',
-      host: 'localhost',
-      port: '27017',
-      name: 'test',
-      user: 'root',
-      pass: '123',
+      provider: '',
+      prefix: '',
+      host: '',
+      port: '',
+      name: '',
+      user: '',
+      pass: '',
     }
   };
