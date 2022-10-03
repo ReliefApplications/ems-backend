c145ebf29d16b8363cb7dfb97dbe54aa9c239475
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-10-03 15:37:53 +0530

    OORT-465 run test CI in the docker-compose

--- config/test.js
+++ config/test.js
@@ -3,14 +3,14 @@
  * Use https://www.npmjs.com/package/config package.
  */
 
- module.exports = {
-    database: {
-      provider: 'docker',
-      prefix: 'mongodb',
-      host: 'localhost',
-      port: '27017',
-      name: 'test',
-      user: 'root',
-      pass: '123',
-    }
-  };
+module.exports = {
+  database: {
+    provider: 'docker',
+    prefix: 'mongodb',
+    host: 'localhost',
+    port: '27017',
+    name: 'test',
+    user: 'root',
+    pass: '123',
+  },
+};
