7e0ff6ca633dc4818c4737026aa5aa6c1665bb08
Antoine Hurard <antoine.reliefapps@gmail.com>
2022-11-03 17:28:14 +0100

    lint

--- src/schema/types/user.type.ts
+++ src/schema/types/user.type.ts
@@ -8,14 +8,7 @@ import {
 import GraphQLJSON from 'graphql-type-json';
 import mongoose from 'mongoose';
 import { ApplicationType, PermissionType, RoleType, GroupType } from '.';
-import {
-  Role,
-  Permission,
-  Application,
-  Resource,
-  Form,
-  Group,
-} from '@models';
+import { Role, Permission, Application, Resource, Form, Group } from '@models';
 import { AppAbility } from '../../security/defineUserAbility';
 import { PositionAttributeType } from './positionAttribute.type';
 import permissions from '../../const/permissions';
