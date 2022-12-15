a26b9e8b417c83d93ca2d27d9754eb4ae85b31b3
ketanreliefapps <ketan.reliefapps@gmail.com>
2022-12-15 15:13:56 +0530

    OORT-547 Add more unit tests for validators methods

--- __tests__/utils/validators/validateApi.test.ts
+++ __tests__/utils/validators/validateApi.test.ts
@@ -1,6 +1,5 @@
 import { validateApi } from '@utils/validators';
 import { faker } from '@faker-js/faker';
-import i18next from 'i18next';
 
 /**
  * Test API validator.
@@ -8,24 +7,18 @@ import i18next from 'i18next';
 describe('API validator tests', () => {
   describe('Correct api name should return true', () => {
     const name = new Array(100).fill(faker.word.adjective());
-    test.each(name)('Random api name should return true', (string: string) => {
-      expect(validateApi(string)).toEqual(undefined);
+    test.each(name)('Random api name should return true', (name: string) => {
+      expect(validateApi(name)).toEqual(undefined);
     });
   });
 
-  /* describe('Random incorrect api should return false', () => {
+  describe('Random incorrect api should return false', () => {
     const strings = new Array(1).fill(faker.internet.email());
     test.each(strings)(
       'Random api name should return false',
       (name: string) => {
-        expect(validateApi(name)).toEqual(
-          expect.arrayContaining([
-            expect.objectContaining({
-              message: i18next.t('errors.invalidGraphQLName'),
-            }),
-          ])
-        );
+        expect(validateApi(name)).toThrow();
       }
     );
-  }); */
+  });
 });
