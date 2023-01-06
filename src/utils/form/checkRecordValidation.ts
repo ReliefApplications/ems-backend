fd9ffcde29ed9dfaa27a92a43ae814c93400b384
Pacôme Rivier <pacome@reliefapplications.org>
2023-01-06 09:59:56 +0100

    Fix 401 errors due to validation with choicesByUrl

--- src/utils/form/checkRecordValidation.ts
+++ src/utils/form/checkRecordValidation.ts
@@ -1,11 +1,31 @@
 import { Form, Record } from 'models';
 import * as Survey from 'survey-knockout';
+import config from 'config';
+
+/**
+ * Pass token before the request to fetch choices by URL if it's targeting SAFE API
+ *
+ * @param context GraphQL context.
+ */
+export const passTokenForChoicesByUrl = (context: any) => {
+  Survey.ChoicesRestfull.onBeforeSendRequest = (
+    sender: Survey.ChoicesRestful,
+    options: { request: XMLHttpRequest }
+  ) => {
+    if (sender.url.includes(config.get('server.url'))) {
+      const token = context.token;
+      options.request.setRequestHeader('Authorization', token);
+    }
+  };
+};
+
 /**
  * Check if the record is correct according to the defined surveyjs validators
  *
  * @param record The record to check
  * @param newData The proposed update
  * @param form The formulaire object linked to the record
+ * @param context GraphQL context
  * @param lang The current language of the form
  * @returns The list of errors (empty if no errors)
  */
@@ -13,8 +33,11 @@ export const checkRecordValidation = (
   record: Record,
   newData: any,
   form: Form,
+  context,
   lang = 'en'
 ): { question: string; errors: string[] }[] => {
+  // Necessary to fix 401 errors if we have choicesByUrl targeting self API.
+  passTokenForChoicesByUrl(context);
   // create the form
   const survey = new Survey.Model(form.structure);
   const onCompleteExpression = survey.toJSON().onCompleteExpression;
