9c809aee8f904d5358f1ac43e3a09a1192877941
ketanreliefapps <ketan.reliefapps@gmail.com>
2023-04-10 17:21:08 +0530

    OORT-609 unit tests for mutations - increase memory size in workflow - ci fail unit test cases

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -3,8 +3,8 @@ import { startDatabase } from '../src/server/database';
 // Execute before each file.
 beforeAll(async () => {
   await startDatabase({ poolSize: 10 });
-});
+}, 15000);
 
 afterAll(async () => {
-  // await stopDatabase();
+  //await stopDatabase();
 });
