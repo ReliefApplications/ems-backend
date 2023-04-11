bed5bb3de50f75bda48b874d1050fbe7654cb7a8
ketanreliefapps <ketan.reliefapps@gmail.com>
2023-04-11 10:45:17 +0530

    OORT-609 unit tests for mutations - add new mutation test cases

--- __tests__/schema/mutation/addPullJob.test.ts
+++ __tests__/schema/mutation/addPullJob.test.ts
@@ -3,12 +3,22 @@ import { SafeTestServer } from '../../server.setup';
 import { faker } from '@faker-js/faker';
 import supertest from 'supertest';
 import { acquireToken } from '../../authentication.setup';
-import { Application, Role, User, Resource, Form } from '@models';
-import { status, contentType } from '@const/enumTypes';
+import {
+  ApiConfiguration,
+  Role,
+  User,
+  Application,
+  Form,
+  Resource,
+  Channel,
+} from '@models';
+import { status } from '@const/enumTypes';
 
 let server: SafeTestServer;
+let apiConfiguration;
 let application;
 let form;
+let channel;
 let request: supertest.SuperTest<supertest.Test>;
 let token: string;
 
@@ -21,22 +31,27 @@ beforeAll(async () => {
   request = supertest(server.app);
   token = `Bearer ${await acquireToken()}`;
 
+  //Create Api Configuration
+  apiConfiguration = await new ApiConfiguration({
+    name: faker.random.alpha(10),
+  }).save();
+
   //Create Application
-  const formName = faker.random.alpha(10);
   application = await new Application({
     name: faker.random.alpha(10),
     status: status.pending,
   }).save();
 
-  //Create Role
-  await new Role({
-    title: faker.random.alpha(10),
-    application: application._id,
-  }).save();
-
   //Create Resource
+  const formName = faker.random.alpha(10);
   const resource = await new Resource({
     name: formName,
+  }).save();
+
+  //Create Channel
+  channel = await new Channel({
+    title: faker.random.alpha(10),
+    application: application._id,
   }).save();
 
   //Create Form
@@ -65,22 +80,51 @@ beforeAll(async () => {
 });
 
 /**
- * Test Add Page Mutation.
+ * Test Add Pull Job Mutation.
  */
-describe('Add page tests cases', () => {
-  const query = `mutation addPage($type: ContentEnumType!, $content: ID, $application: ID!) {
-    addPage(type: $type, content: $content, application: $application){
+describe('Add pull job tests cases', () => {
+  const query = `mutation addPullJob($name: String!,$status: Status!, $apiConfiguration: ID!, $url: String, $path: String, $schedule: String, $convertTo: ID, $mapping: JSON, $uniqueIdentifiers: [String], $channel: ID ) {
+    addPullJob(name: $name, status: $status, apiConfiguration: $apiConfiguration, url: $url, path: $path, schedule: $schedule, convertTo: $convertTo, mapping: $mapping, uniqueIdentifiers: $uniqueIdentifiers, channel : $channel){
       id
       name
-      type
     }
   }`;
 
-  test('test case add page tests with correct data', async () => {
+  test('test case add pull job tests with correct data', async () => {
     const variables = {
-      type: contentType.form,
-      content: form._id,
-      application: application._id,
+      name: faker.random.alpha(10),
+      status: status.active,
+      apiConfiguration: apiConfiguration._id,
+      url: faker.internet.url(),
+      path: faker.system.directoryPath(),
+      schedule: '0 12 5 7 *',
+      convertTo: form._id,
+      mapping: faker.datatype.json(),
+      uniqueIdentifiers: [faker.random.alpha(10), faker.random.alpha(10)],
+      channel: channel._id,
+    };
+
+    const response = await request
+      .post('/graphql')
+      .send({ query, variables })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('data');
+    expect(response.body).not.toHaveProperty('errors');
+    expect(response.body.data.addPullJob).toHaveProperty('id');
+  });
+
+  test('test case with wrong name and return error', async () => {
+    const variables = {
+      name: faker.science.unit(),
+      status: status.active,
+      apiConfiguration: apiConfiguration._id,
+      url: faker.internet.url(),
+      path: faker.system.directoryPath(),
+      schedule: '0 12 5 7 *',
+      mapping: faker.datatype.json(),
+      uniqueIdentifiers: faker.random.alpha(10),
     };
 
     const response = await request
@@ -92,35 +136,18 @@ describe('Add page tests cases', () => {
       expect(
         Promise.reject(new Error(response.body.errors[0].message))
       ).rejects.toThrow(response.body.errors[0].message);
-    } else {
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('data');
-      expect(response.body).not.toHaveProperty('errors');
-      expect(response.body.data.addPage).toHaveProperty('id');
     }
   });
 
-  test('test case with wrong type and return error', async () => {
+  test('test case without name and return error', async () => {
     const variables = {
-      type: faker.science.unit(),
-      application: application._id,
-    };
-
-    const response = await request
-      .post('/graphql')
-      .send({ query, variables })
-      .set('Authorization', token)
-      .set('Accept', 'application/json');
-    if (!!response.body.errors && !!response.body.errors[0].message) {
-      expect(
-        Promise.reject(new Error(response.body.errors[0].message))
-      ).rejects.toThrow(response.body.errors[0].message);
-    }
-  });
-
-  test('test case without type and return error', async () => {
-    const variables = {
-      application: application._id,
+      status: status.active,
+      apiConfiguration: apiConfiguration._id,
+      url: faker.internet.url(),
+      path: faker.system.directoryPath(),
+      schedule: '0 12 5 7 *',
+      mapping: faker.datatype.json(),
+      uniqueIdentifiers: faker.random.alpha(10),
     };
 
     const response = await request
