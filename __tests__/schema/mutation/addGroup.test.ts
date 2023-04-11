8deeba2e1de864baa422582ee0eb0b71df11376c
ketanreliefapps <ketan.reliefapps@gmail.com>
2023-04-11 10:37:32 +0530

    OORT-609 unit tests for mutations - add new mutation test cases

--- __tests__/schema/mutation/addGroup.test.ts
+++ __tests__/schema/mutation/addGroup.test.ts
@@ -43,10 +43,16 @@ describe('Add group tests cases', () => {
         .send({ query, variables })
         .set('Authorization', token)
         .set('Accept', 'application/json');
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty('data');
-      expect(response.body).not.toHaveProperty('errors');
-      expect(response.body.data.addGroup).toHaveProperty('id');
+      if (!!response.body.errors && !!response.body.errors[0].message) {
+        expect(
+          Promise.reject(new Error(response.body.errors[0].message))
+        ).rejects.toThrow(response.body.errors[0].message);
+      } else {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('data');
+        expect(response.body).not.toHaveProperty('errors');
+        expect(response.body.data.addGroup).toHaveProperty('id');
+      }
     }
   });
 
--- __tests__/schema/mutation/addPositionAttributeCategory.test.ts
+++ __tests__/schema/mutation/addPositionAttributeCategory.test.ts
@@ -55,12 +55,18 @@ describe('Add position attribute category mutation tests cases', () => {
       .send({ query, variables })
       .set('Authorization', token)
       .set('Accept', 'application/json');
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty('data');
-    expect(response.body).not.toHaveProperty('errors');
-    expect(response.body.data.addPositionAttributeCategory).toHaveProperty(
-      'id'
-    );
+    if (!!response.body.errors && !!response.body.errors[0].message) {
+      expect(
+        Promise.reject(new Error(response.body.errors[0].message))
+      ).rejects.toThrow(response.body.errors[0].message);
+    } else {
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('data');
+      expect(response.body).not.toHaveProperty('errors');
+      expect(response.body.data.addPositionAttributeCategory).toHaveProperty(
+        'id'
+      );
+    }
   });
 
   test('test case with wrong title and return error', async () => {
