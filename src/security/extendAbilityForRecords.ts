35ae108b9ac21aaabdeb62359adc8a232c86d770
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-05-19 18:49:11 +0400

    add comments about last piece of removed code

--- src/security/extendAbilityForRecords.ts
+++ src/security/extendAbilityForRecords.ts
@@ -160,6 +160,8 @@ function extendAbilityForRecordsOnForm(
 
     // access a record
     if (userHasRoleFor('canSeeRecords', user, resource)) {
+      // can('read', 'Form', { _id: form._id });
+      // can('read', 'Resource', { _id: resource._id });
       const filter = formFilters('canSeeRecords', user, resource);
       can('read', 'Record', filter);
       cannot('read', 'Record', ['data.**'], filter);
