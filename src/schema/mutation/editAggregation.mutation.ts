d597058f6dd7a9e0a75e17bbc325d32fe485a694
ketanreliefapps <ketan.reliefapps@gmail.com>
2023-05-22 17:24:08 +0530

    AB#11064 prevent admin to remove fields that are used on grids or workflow or pull jobs

--- src/schema/mutation/editAggregation.mutation.ts
+++ src/schema/mutation/editAggregation.mutation.ts
@@ -1,9 +1,9 @@
 import { GraphQLError, GraphQLID, GraphQLNonNull } from 'graphql';
-import { Dashboard, Resource } from '@models';
+import { Resource } from '@models';
 import { AggregationType } from '../../schema/types';
 import { AppAbility } from '@security/defineUserAbility';
 import AggregationInputType from '../../schema/inputs/aggregation.input';
-// import { logger } from '@services/logger.service';
+import { logger } from '@services/logger.service';
 
 /**
  * Edit existing aggregation.
@@ -17,89 +17,47 @@ export default {
     resource: { type: GraphQLID },
   },
   async resolve(parent, args, context) {
-    // try {
-    if (!args.resource || !args.aggregation) {
-      throw new GraphQLError(
-        context.i18next.t('mutations.aggregation.edit.errors.invalidArguments')
-      );
-    }
-
-    const user = context.user;
-    if (!user) {
-      throw new GraphQLError(context.i18next.t('common.errors.userNotLogged'));
-    }
-    const ability: AppAbility = user.ability;
-    // Edition of a resource
-    if (args.resource) {
-      //get resource aggregation data
-      const resourceData = await Resource.findOne({
-        aggregations: {
-          $elemMatch: { _id: args.id },
-        },
-      });
-
-      if (!args.forceFullyUpdateFields) {
-        args.forceFullyUpdateFields = false;
-      }
-      //check aggregation data available or not and check forcefully update this aggregation fields..
-      if (
-        !!resourceData &&
-        !!resourceData.aggregations &&
-        args.forceFullyUpdateFields == false
-      ) {
-        //get widget data
-        const widgetData = await Dashboard.find({
-          $and: [
-            {
-              structure: {
-                $elemMatch: {
-                  'settings.resource': resourceData._id.toString(),
-                },
-              },
-            },
-            {
-              structure: {
-                $elemMatch: { 'settings.aggregations': args.id.toString() },
-              },
-            },
-          ],
-        });
-
-        //check widget data available or not.
-        if (!!widgetData) {
-          throw new GraphQLError(
-            context.i18next.t(
-              'mutations.aggregation.edit.errors.alreadyUseAggregationField',
-              {
-                name: 'widget',
-              }
-            )
-          );
-        }
-      }
-      const filters = Resource.accessibleBy(ability, 'update')
-        .where({ _id: args.resource })
-        .getFilter();
-      const resource: Resource = await Resource.findOne(filters);
-      if (!resource) {
+    try {
+      if (!args.resource || !args.aggregation) {
         throw new GraphQLError(
-          context.i18next.t('common.errors.permissionNotGranted')
+          context.i18next.t(
+            'mutations.aggregation.edit.errors.invalidArguments'
+          )
         );
       }
-      resource.aggregations.id(args.id).sourceFields =
-        args.aggregation.sourceFields;
-      resource.aggregations.id(args.id).pipeline = args.aggregation.pipeline;
-      resource.aggregations.id(args.id).mapping = args.aggregation.mapping;
-      resource.aggregations.id(args.id).name = args.aggregation.name;
 
-      await resource.save();
-      return resource.aggregations.id(args.id);
+      const user = context.user;
+      if (!user) {
+        throw new GraphQLError(
+          context.i18next.t('common.errors.userNotLogged')
+        );
+      }
+      const ability: AppAbility = user.ability;
+      // Edition of a resource
+      if (args.resource) {
+        const filters = Resource.accessibleBy(ability, 'update')
+          .where({ _id: args.resource })
+          .getFilter();
+        const resource: Resource = await Resource.findOne(filters);
+        if (!resource) {
+          throw new GraphQLError(
+            context.i18next.t('common.errors.permissionNotGranted')
+          );
+        }
+        resource.aggregations.id(args.id).sourceFields =
+          args.aggregation.sourceFields;
+        resource.aggregations.id(args.id).pipeline = args.aggregation.pipeline;
+        resource.aggregations.id(args.id).mapping = args.aggregation.mapping;
+        resource.aggregations.id(args.id).name = args.aggregation.name;
+
+        await resource.save();
+        return resource.aggregations.id(args.id);
+      }
+    } catch (err) {
+      logger.error(err.message, { stack: err.stack });
+      throw new GraphQLError(
+        context.i18next.t('common.errors.internalServerError')
+      );
     }
-    // } catch (err) {
-    //   logger.error(err.message, { stack: err.stack });
-    //   throw new GraphQLError(
-    //     context.i18next.t('common.errors.internalServerError')
-    //   );
-    // }
   },
 };
--- src/schema/mutation/editLayout.mutation.ts
+++ src/schema/mutation/editLayout.mutation.ts
@@ -1,9 +1,9 @@
 import { GraphQLError, GraphQLID, GraphQLNonNull } from 'graphql';
-import { Resource, Form, Dashboard } from '@models';
+import { Resource, Form } from '@models';
 import { LayoutType } from '../../schema/types';
 import { AppAbility } from '@security/defineUserAbility';
 import LayoutInputType from '../../schema/inputs/layout.input';
-// import { logger } from '@services/logger.service';
+import { logger } from '@services/logger.service';
 
 /**
  * Edits an existing layout.
@@ -17,104 +17,60 @@ export default {
     form: { type: GraphQLID },
   },
   async resolve(parent, args, context) {
-    // try {
-    if (args.form && args.resource) {
-      throw new GraphQLError(
-        context.i18next.t(
-          'mutations.layout.edit.errors.invalidAddPageArguments'
-        )
-      );
-    }
-    const user = context.user;
-    if (!user) {
-      throw new GraphQLError(context.i18next.t('common.errors.userNotLogged'));
-    }
-
-    const ability: AppAbility = user.ability;
-    // Edition of a resource
-    if (args.resource) {
-      //get resource layout data
-      const resourceData = await Resource.findOne({
-        layouts: {
-          $elemMatch: { _id: args.id },
-        },
-      });
-
-      if (!args.forceFullyUpdateFields) {
-        args.forceFullyUpdateFields = false;
+    try {
+      if (args.form && args.resource) {
+        throw new GraphQLError(
+          context.i18next.t(
+            'mutations.layout.edit.errors.invalidAddPageArguments'
+          )
+        );
+      }
+      const user = context.user;
+      if (!user) {
+        throw new GraphQLError(
+          context.i18next.t('common.errors.userNotLogged')
+        );
       }
 
-      //check layout data available or not and check forcefully update this layout fields..
-      if (
-        !!resourceData &&
-        !!resourceData.layouts &&
-        args.forceFullyUpdateFields == false
-      ) {
-        //get widget data
-        const widgetData = await Dashboard.find({
-          $and: [
-            {
-              structure: {
-                $elemMatch: {
-                  'settings.resource': resourceData._id.toString(),
-                },
-              },
-            },
-            {
-              structure: {
-                $elemMatch: { 'settings.layouts': args.id.toString() },
-              },
-            },
-          ],
-        });
-
-        if (!!widgetData) {
+      const ability: AppAbility = user.ability;
+      // Edition of a resource
+      if (args.resource) {
+        const filters = Resource.accessibleBy(ability, 'update')
+          .where({ _id: args.resource })
+          .getFilter();
+        const resource: Resource = await Resource.findOne(filters);
+        if (!resource) {
           throw new GraphQLError(
-            context.i18next.t(
-              'mutations.layout.edit.errors.alreadyUseLayoutField',
-              {
-                name: 'widget',
-              }
-            )
+            context.i18next.t('common.errors.permissionNotGranted')
           );
         }
+        resource.layouts.id(args.id).name = args.layout.name;
+        resource.layouts.id(args.id).query = args.layout.query;
+        resource.layouts.id(args.id).display = args.layout.display;
+        await resource.save();
+        return resource.layouts.id(args.id);
+      } else {
+        // Edition of a Form
+        const filters = Form.accessibleBy(ability, 'update')
+          .where({ _id: args.form })
+          .getFilter();
+        const form: Form = await Form.findOne(filters);
+        if (!form) {
+          throw new GraphQLError(
+            context.i18next.t('common.errors.permissionNotGranted')
+          );
+        }
+        form.layouts.id(args.id).name = args.layout.name;
+        form.layouts.id(args.id).query = args.layout.query;
+        form.layouts.id(args.id).display = args.layout.display;
+        await form.save();
+        return form.layouts.id(args.id);
       }
-      const filters = Resource.accessibleBy(ability, 'update')
-        .where({ _id: args.resource })
-        .getFilter();
-      const resource: Resource = await Resource.findOne(filters);
-      if (!resource) {
-        throw new GraphQLError(
-          context.i18next.t('common.errors.permissionNotGranted')
-        );
-      }
-      resource.layouts.id(args.id).name = args.layout.name;
-      resource.layouts.id(args.id).query = args.layout.query;
-      resource.layouts.id(args.id).display = args.layout.display;
-      await resource.save();
-      return resource.layouts.id(args.id);
-    } else {
-      // Edition of a Form
-      const filters = Form.accessibleBy(ability, 'update')
-        .where({ _id: args.form })
-        .getFilter();
-      const form: Form = await Form.findOne(filters);
-      if (!form) {
-        throw new GraphQLError(
-          context.i18next.t('common.errors.permissionNotGranted')
-        );
-      }
-      form.layouts.id(args.id).name = args.layout.name;
-      form.layouts.id(args.id).query = args.layout.query;
-      form.layouts.id(args.id).display = args.layout.display;
-      await form.save();
-      return form.layouts.id(args.id);
+    } catch (err) {
+      logger.error(err.message, { stack: err.stack });
+      throw new GraphQLError(
+        context.i18next.t('common.errors.internalServerError')
+      );
     }
-    // } catch (err) {
-    //   logger.error(err.message, { stack: err.stack });
-    //   throw new GraphQLError(
-    //     context.i18next.t('common.errors.internalServerError')
-    //   );
-    // }
   },
 };
