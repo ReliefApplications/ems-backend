cd4f1a441ea748f90102f07a48ce0b9ae837a4cc
Antoine Hurard <antoine.reliefapps@gmail.com>
2023-06-05 11:20:34 +0400

    allow ping url to be called to test api proxy before saving all parameters "

--- src/routes/proxy/index.ts
+++ src/routes/proxy/index.ts
@@ -3,42 +3,40 @@ import { request as httpRequest } from 'http';
 import { request as httpsRequest } from 'https';
 import { ApiConfiguration } from '@models';
 import { getToken } from '@utils/proxy';
-import { isEmpty } from 'lodash';
+import { get, isEmpty } from 'lodash';
 import i18next from 'i18next';
 import { logger } from '@services/logger.service';
+import config from 'config';
+import * as CryptoJS from 'crypto-js';
 
 /** Express router */
 const router = express.Router();
 
+/** Placeholder to hide settings in UI once saved */
+const SETTING_PLACEHOLDER = '●●●●●●●●●●●●●';
+
 /**
- * Forward requests to actual API using the API Configuration
+ * Proxy API request
+ *
+ * @param req request
+ * @param res response
+ * @param api api configuration
+ * @param path url path
+ * @returns API request
  */
-router.all('/:name/**', async (req, res) => {
+const proxyAPIRequest = async (req, res, api, path) => {
   try {
     req.pause();
-    const apiConfiguration = await ApiConfiguration.findOne({
-      $or: [{ name: req.params.name }, { id: req.params.name }],
-      status: 'active',
-    }).select('name authType endpoint settings id');
-    if (!apiConfiguration) {
-      return res.status(404).send(i18next.t('common.errors.dataNotFound'));
-    }
-    const token = await getToken(apiConfiguration);
+    const token = await getToken(api);
     const headers = Object.assign(req.headers, {
       authorization: `Bearer ${token}`,
     });
-    const endpoint = req.originalUrl.split(req.params.name).pop().substring(1);
     // Add / between endpoint and path, and ensure that double slash are removed
     const url = new URL(
-      `${apiConfiguration.endpoint.replace(/\$/, '')}/${endpoint}`.replace(
-        /([^:]\/)\/+/g,
-        '$1'
-      )
+      `${api.endpoint.replace(/\$/, '')}/${path}`.replace(/([^:]\/)\/+/g, '$1')
     );
     headers.host = url.hostname;
-    const protocol = apiConfiguration.endpoint.startsWith('https')
-      ? 'https:'
-      : 'http:';
+    const protocol = api.endpoint.startsWith('https') ? 'https:' : 'http:';
     const options: any = {
       protocol,
       host: url.hostname,
@@ -121,6 +119,85 @@ router.all('/:name/**', async (req, res) => {
       logger.error(err.message, { stack: err.stack });
       return res.status(503).send('Service currently unavailable');
     }
+  } catch (err) {
+    logger.error(err.message, { stack: err.stack });
+    return res.status(500).send(req.t('common.errors.internalServerError'));
+  }
+};
+
+router.post('/ping/**', async (req, res) => {
+  try {
+    const body = req.body;
+    if (body) {
+      const api = await ApiConfiguration.findById(req.body.id).select(
+        'name authType endpoint pingUrl settings id'
+      );
+      if (!api) {
+        return res.status(404).send(i18next.t('common.errors.dataNotFound'));
+      }
+      // Decrypt settings
+      const settings: {
+        authTargetUrl: string;
+        apiClientID: string;
+        safeSecret: string;
+        scope: string;
+      } = !isEmpty(api.settings)
+        ? JSON.parse(
+            CryptoJS.AES.decrypt(
+              api.settings,
+              config.get('encryption.key')
+            ).toString(CryptoJS.enc.Utf8)
+          )
+        : {};
+      const parameters = {
+        ...body,
+        settings: {
+          authTargetUrl:
+            get(body, 'settings.authTargetUrl', SETTING_PLACEHOLDER) !==
+            SETTING_PLACEHOLDER
+              ? get(body, 'settings.authTargetUrl')
+              : get(settings, 'authTargetUrl'),
+          apiClientId:
+            get(body, 'settings.apiClientId', SETTING_PLACEHOLDER) !==
+            SETTING_PLACEHOLDER
+              ? get(body, 'settings.apiClientId')
+              : get(settings, 'apiClientId'),
+          safeSecret:
+            get(body, 'settings.safeSecret', SETTING_PLACEHOLDER) !==
+            SETTING_PLACEHOLDER
+              ? get(body, 'settings.safeSecret')
+              : get(settings, 'safeSecret'),
+          scope:
+            get(body, 'settings.scope', SETTING_PLACEHOLDER) !==
+            SETTING_PLACEHOLDER
+              ? get(body, 'settings.scope')
+              : get(settings, 'scope'),
+        },
+      };
+
+      req.method = 'GET';
+      proxyAPIRequest(req, res, parameters, api.pingUrl);
+    }
+  } catch (err) {
+    logger.error(err.message, { stack: err.stack });
+    return res.status(500).send(req.t('common.errors.internalServerError'));
+  }
+});
+
+/**
+ * Forward requests to actual API using the API Configuration
+ */
+router.all('/:name/**', async (req, res) => {
+  try {
+    const api = await ApiConfiguration.findOne({
+      $or: [{ name: req.params.name }, { id: req.params.name }],
+      status: 'active',
+    }).select('name authType endpoint settings id');
+    if (!api) {
+      return res.status(404).send(i18next.t('common.errors.dataNotFound'));
+    }
+    const path = req.originalUrl.split(req.params.name).pop().substring(1);
+    proxyAPIRequest(req, res, api, path);
   } catch (err) {
     logger.error(err.message, { stack: err.stack });
     return res.status(500).send(req.t('common.errors.internalServerError'));
