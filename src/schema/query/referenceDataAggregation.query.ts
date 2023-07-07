033f9e9a7356337e8ee1baf9fe3085e0ae33d5f0
MwanPygmay <matt.brunot@laposte.net>
2023-07-07 16:47:31 +0200

    changed aggregations so it looks more like the records one, still not the end though

--- src/schema/query/referenceDataAggregation.query.ts
+++ src/schema/query/referenceDataAggregation.query.ts
@@ -1,12 +1,41 @@
-import { GraphQLError, GraphQLID, GraphQLNonNull } from 'graphql';
+import { GraphQLError, GraphQLID, GraphQLInt, GraphQLNonNull } from 'graphql';
 import GraphQLJSON from 'graphql-type-json';
 import { ReferenceData } from '@models';
 import getFilteredArray from '@utils/schema/resolvers/Query/getFilteredArray';
 import { logger } from '@services/logger.service';
 import checkPageSize from '@utils/schema/errors/checkPageSize.util';
+import { isEqual } from 'lodash';
 
 /** Pagination default items per query */
 const DEFAULT_FIRST = 10;
+
+/**
+ * returns the result for a pipeline step
+ *
+ * @param pipelineStep step of the pipeline to build a result from
+ * @param data the reference data
+ * @returns filtered data
+ */
+const procPipelineStep = (pipelineStep, data) => {
+  switch (pipelineStep.type) {
+    case 'group':
+      return data.reduce(function (r, a) {
+        const key = a[pipelineStep.form.groupBy];
+        r[key] = r[key] || [];
+        r[key].push(a);
+        return r;
+      }, Object.create(null));
+    case 'filter':
+      return getFilteredArray(data, pipelineStep.filter);
+    case 'sort':
+      if (pipelineStep.form.order === 'desc')
+        return data.sort((a, b) => (a.type < b.type ? -1 : 1));
+      else if (pipelineStep.order === 'asc')
+        return data.sort((a, b) => (a.type < b.type ? 1 : -1));
+    default:
+      console.error('Aggregation error or not supported yet');
+  }
+};
 
 /**
  * Take an aggregation configuration as parameter.
@@ -16,7 +45,10 @@ export default {
   type: GraphQLJSON,
   args: {
     referenceData: { type: new GraphQLNonNull(GraphQLID) },
-    aggregation: { type: new GraphQLNonNull(GraphQLJSON) },
+    aggregation: { type: new GraphQLNonNull(GraphQLID) },
+    mapping: { type: GraphQLJSON },
+    first: { type: GraphQLInt },
+    skip: { type: GraphQLInt },
   },
   async resolve(parent, args, context) {
     // Make sure that the page size is not too important
@@ -33,7 +65,10 @@ export default {
 
       const referenceData = await ReferenceData.findById(args.referenceData);
 
-      const aggregation = args.aggregation;
+      // As we only queried one aggregation
+      const aggregation = referenceData.aggregations.find((x) =>
+        isEqual(x.id, args.aggregation)
+      );
 
       // Check if resource exists and aggregation exists
       if (!(referenceData && aggregation && referenceData.data)) {
@@ -44,31 +79,18 @@ export default {
       if (
         aggregation.sourceFields &&
         aggregation.sourceFields.length &&
-        aggregation.pipeline &&
-        aggregation.pipeline.type
+        aggregation.pipeline
       ) {
-        switch (aggregation.pipeline.type) {
-          case 'group':
-            return referenceData.data.reduce(function (r, a) {
-              const key = a[aggregation.pipeline.groupBy];
-              r[key] = r[key] || [];
-              r[key].push(a);
-              return r;
-            }, Object.create(null));
-          case 'filter':
-            return getFilteredArray(
-              referenceData.data,
-              aggregation.pipeline.filter
-            );
-          case 'sort':
-            if (aggregation.pipeline.order === 'desc')
-              return referenceData.data.sort((a, b) =>
-                a.type < b.type ? -1 : 1
-              );
-            else if (aggregation.pipeline.order === 'asc')
-              return referenceData.data.sort((a, b) =>
-                a.type < b.type ? 1 : -1
-              );
+        try {
+          let dataToAggregate = referenceData.data;
+          aggregation.pipeline.forEach((step: any) => {
+            dataToAggregate = procPipelineStep(step, dataToAggregate);
+          });
+          return { items: dataToAggregate, totalCount: dataToAggregate.length };
+        } catch (error) {
+          throw new GraphQLError(
+            'Something went wrong with the pipelines, these aggregations may not be supported yet'
+          );
         }
       } else {
         throw new GraphQLError(
