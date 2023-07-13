843a6d46fb4e76f73dfb9f711b61aa8201316ec8
ketanreliefapps <ketan.reliefapps@gmail.com>
2023-07-13 15:20:30 +0530

    add new mutation test cases

--- __tests__/schema/query/layers.test.ts
+++ __tests__/schema/query/layers.test.ts
@@ -10,6 +10,17 @@ let server: ApolloServer;
  */
 describe('Layers query tests', () => {
   const query = '{ layers { id, name } }';
+
+  test('query with wrong user returns error', async () => {
+    server = await SafeTestServer.createApolloTestServer(schema, {
+      name: 'Wrong user',
+      roles: [],
+    });
+    const result = await server.executeOperation({ query });
+
+    expect(result).toHaveProperty(['data', 'layers']);
+    expect(result.data?.layers).toEqual(null);
+  });
   test('query with admin user returns expected number of layers', async () => {
     const count = await Layer.countDocuments();
     const admin = await Role.findOne(
