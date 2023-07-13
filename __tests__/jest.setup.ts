0267ff390286b185d0f207dd3047825bbbc2dd9d
ketanreliefapps <ketan.reliefapps@gmail.com>
2023-07-13 16:55:13 +0530

    add new mutation test cases

--- __tests__/jest.setup.ts
+++ __tests__/jest.setup.ts
@@ -3,7 +3,7 @@ import { startDatabase } from '../src/server/database';
 // Execute before each file.
 beforeAll(async () => {
   await startDatabase({ poolSize: 10 });
-}, 50000);
+}, 35000);
 
 afterAll(async () => {
   //await stopDatabase();
