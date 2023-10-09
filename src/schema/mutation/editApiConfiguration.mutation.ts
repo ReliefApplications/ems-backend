33b79010f9fa3b4602e75c3c01da09b74a584483
unai-reliefapp <123092672+unai-reliefapp@users.noreply.github.com>
2023-10-09 12:14:57 +0200

    fix: unfriendly api configuration edition (#766)
    ---------
    
    Co-authored-by: Antoine Hurard <antoine.reliefapps@gmail.com>

--- src/schema/mutation/editApiConfiguration.mutation.ts
+++ src/schema/mutation/editApiConfiguration.mutation.ts
@@ -15,6 +15,7 @@ import config from 'config';
 import { logger } from '@services/logger.service';
 import { accessibleBy } from '@casl/mongoose';
 import { graphQLAuthCheck } from '@schema/shared';
+import { isEmpty } from 'lodash';
 
 /**
  * Edit the passed apiConfiguration if authorized.
@@ -66,12 +67,6 @@ export default {
         args.endpoint && { endpoint: args.endpoint },
         args.graphQLEndpoint && { graphQLEndpoint: args.graphQLEndpoint },
         args.pingUrl && { pingUrl: args.pingUrl },
-        args.settings && {
-          settings: CryptoJS.AES.encrypt(
-            JSON.stringify(args.settings),
-            config.get('encryption.key')
-          ).toString(),
-        },
         args.permissions && { permissions: args.permissions }
       );
       const filters = ApiConfiguration.find(
@@ -79,13 +74,37 @@ export default {
       )
         .where({ _id: args.id })
         .getFilter();
-      const apiConfiguration = await ApiConfiguration.findOneAndUpdate(
-        filters,
-        update,
-        { new: true }
-      );
-      if (apiConfiguration) {
-        return apiConfiguration;
+      const api = await ApiConfiguration.findOne(filters);
+      if (api) {
+        if (args.settings) {
+          // Merge old settings & new settings to prevent some fields to be lost
+          const prevSettings = !isEmpty(api.settings)
+            ? JSON.parse(
+                CryptoJS.AES.decrypt(
+                  api.settings,
+                  config.get('encryption.key')
+                ).toString(CryptoJS.enc.Utf8)
+              )
+            : {};
+          console.log({
+            ...prevSettings,
+            ...args.settings,
+          });
+          // Encrypt again and add to the new update object, merging old and new settings
+          Object.assign(update, {
+            settings: CryptoJS.AES.encrypt(
+              JSON.stringify({
+                ...prevSettings,
+                ...args.settings,
+              }),
+              config.get('encryption.key')
+            ).toString(),
+          });
+        }
+
+        return await ApiConfiguration.findOneAndUpdate(filters, update, {
+          new: true,
+        });
       } else {
         throw new GraphQLError(
           context.i18next.t('common.errors.permissionNotGranted')
