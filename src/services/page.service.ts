30a5a030e46d420b66a2aa90e165366a00c8af05
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2023-10-18 20:18:46 -0300

    fix: Sequence of pages and steps is being changed.

--- src/services/page.service.ts
+++ src/services/page.service.ts
@@ -10,27 +10,22 @@ import i18next from 'i18next';
  */
 export const duplicatePages = async (application: Application) => {
   const copiedPages = [];
-  await Promise.all(
-    application.pages.map(async (pageId) => {
-      await Page.findById(pageId).then(async (p) => {
-        if (p && p.archived === false) {
-          const page = new Page({
-            name: p.name,
-            createdAt: new Date(),
-            type: p.type,
-            // eslint-disable-next-line @typescript-eslint/no-use-before-define
-            content: await duplicateContent(p.content, p.type),
-            permissions: p.permissions,
-          });
-          const id = await page.save().then((saved) => {
-            copiedPages.push(saved.id);
-            return saved.id;
-          });
-          return id;
-        }
+  for (const pageId of application.pages) {
+    const p = await Page.findById(pageId);
+    if (p && p.archived === false) {
+      const page = new Page({
+        name: p.name,
+        createdAt: new Date(),
+        type: p.type,
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        content: await duplicateContent(p.content, p.type),
+        permissions: p.permissions,
+        icon: p.icon,
       });
-    })
-  );
+      const saved = await page.save();
+      copiedPages.push(saved.id);
+    }
+  }
   return copiedPages;
 };
 
@@ -123,31 +118,21 @@ const duplicateContent = async (
  */
 const duplicateSteps = async (ids, permissions?: any): Promise<any[]> => {
   const copiedSteps = [];
-  await Promise.all(
-    ids.map(async (id) => {
-      await Step.findById(id).then(async (s) => {
-        if (s.type !== 'workflow') {
-          //A step type should never be workflow, but if some error occurs, this condition will prevent recursion
-          const step = new Step({
-            name: s.name,
-            createdAt: new Date(),
-            type: s.type,
-            content: await duplicateContent(
-              s.content,
-              s.type,
-              null,
-              permissions
-            ),
-            permissions: permissions || s.permissions,
-          });
-          const newId = await step.save().then((saved) => {
-            copiedSteps.push(saved.id);
-            return saved.id;
-          });
-          return newId;
-        }
+  for (const id of ids) {
+    const s = await Step.findById(id);
+    if (s.type !== 'workflow') {
+      //A step type should never be workflow, but if some error occurs, this condition will prevent recursion
+      const step = new Step({
+        name: s.name,
+        createdAt: new Date(),
+        type: s.type,
+        content: await duplicateContent(s.content, s.type, null, permissions),
+        permissions: permissions || s.permissions,
+        icon: s.icon,
       });
-    })
-  );
+      const saved = await step.save();
+      copiedSteps.push(saved.id);
+    }
+  }
   return copiedSteps;
 };
