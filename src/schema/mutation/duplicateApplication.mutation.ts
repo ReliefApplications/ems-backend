ab0c83951b51dadc9d17808fcbe3cfc31cc2dc76
estelafs <estelafersil@gmail.com>
2023-10-27 15:50:26 -0300

    improved error message when trying to duplicate application with existing name

--- src/schema/mutation/duplicateApplication.mutation.ts
+++ src/schema/mutation/duplicateApplication.mutation.ts
@@ -70,6 +70,18 @@ export default {
       if (ability.can('create', 'Application')) {
         // Check that a name was provided for the application
         if (args.name !== '') {
+          //  check if name is unique
+          const applicationWithName = await Application.findOne({
+            name: args.name,
+          });
+          if (applicationWithName) {
+            throw new GraphQLError(
+              context.i18next.t(
+                'mutations.application.duplicate.errors.namedDuplicated',
+                { name: args.name }
+              )
+            );
+          }
           // Copy application, and its permissions
           const application = new Application({
             name: args.name,
@@ -85,6 +97,7 @@ export default {
             contextualFilter: baseApplication.contextualFilter,
             contextualFilterPosition: baseApplication.contextualFilterPosition,
           });
+          console.log('1');
           // Copy files from base application
           await copyFolder('applications', baseApplication.id, application.id);
           // Make sure the new application uses the correct css file
