6298ce474718a9bc45f2d72eb0372fbb5432b291
Pacôme Rivier <matt.brunot@laposte.net>
2023-11-23 16:38:42 +0100

    oopsi readd pulljobs

--- src/server/pullJobScheduler.ts
+++ src/server/pullJobScheduler.ts
@@ -39,7 +39,7 @@ const pullJobScheduler = async () => {
 
   for (const pullJob of pullJobs) {
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    // scheduleJob(pullJob);
+    scheduleJob(pullJob);
   }
 };
 
