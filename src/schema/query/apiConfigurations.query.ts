ab8eeaa2f7e0deb378504f3cc1d90428fa606984
Unai Zalba <unai@reliefapplications.org>
2023-12-04 17:18:29 +0100

    bugfix/AB#80663_ABC-incorrect-API-configuration-and-reference-data-view feat: add sort by name feature to api configurations feat: add filters by status and name to the api configurations

--- src/schema/query/apiConfigurations.query.ts
+++ src/schema/query/apiConfigurations.query.ts
@@ -1,9 +1,12 @@
-import { GraphQLError, GraphQLInt, GraphQLID } from 'graphql';
+import { GraphQLError, GraphQLInt, GraphQLID, GraphQLString } from 'graphql';
 import {
   ApiConfigurationConnectionType,
   encodeCursor,
   decodeCursor,
 } from '../types';
+import GraphQLJSON from 'graphql-type-json';
+import getFilter from '@utils/filter/getFilter';
+import getSortOrder from '@utils/schema/resolvers/Query/getSortOrder';
 import { ApiConfiguration } from '@models';
 import { AppAbility } from '@security/defineUserAbility';
 import { logger } from '@services/logger.service';
@@ -11,6 +14,7 @@ import checkPageSize from '@utils/schema/errors/checkPageSize.util';
 import { accessibleBy } from '@casl/mongoose';
 import { graphQLAuthCheck } from '@schema/shared';
 import { Context } from '@server/apollo/context';
+import { CompositeFilterDescriptor } from '@const/compositeFilter';
 
 /** Default page size */
 const DEFAULT_FIRST = 10;
@@ -19,7 +23,43 @@ const DEFAULT_FIRST = 10;
 type ApiConfigurationsArgs = {
   first?: number;
   afterCursor?: string;
+  filter?: CompositeFilterDescriptor;
+  sortField?: string;
+  sortOrder?: string;
 };
+
+/** Available filter fields */
+const FILTER_FIELDS: { name: string; type: string }[] = [
+  {
+    name: 'status',
+    type: 'text',
+  },
+  {
+    name: 'name',
+    type: 'text',
+  },
+];
+
+/** Available sort fields */
+const SORT_FIELDS = [
+  {
+    name: 'name',
+    cursorId: (node: any) => node.name,
+    cursorFilter: (cursor: any, sortOrder: string) => {
+      const operator = sortOrder === 'asc' ? '$gt' : '$lt';
+      return {
+        name: {
+          [operator]: decodeCursor(cursor),
+        },
+      };
+    },
+    sort: (sortOrder: string) => {
+      return {
+        name: getSortOrder(sortOrder),
+      };
+    },
+  },
+];
 
 /**
  * List all apiConfiguration available for the logged user.
@@ -30,6 +70,9 @@ export default {
   args: {
     first: { type: GraphQLInt },
     afterCursor: { type: GraphQLID },
+    filter: { type: GraphQLJSON },
+    sortField: { type: GraphQLString },
+    sortOrder: { type: GraphQLString },
   },
   async resolve(parent, args: ApiConfigurationsArgs, context: Context) {
     graphQLAuthCheck(context);
@@ -37,24 +80,34 @@ export default {
     const first = args.first || DEFAULT_FIRST;
     checkPageSize(first);
     try {
+      // Inputs check
+      if (args.sortField) {
+        if (!SORT_FIELDS.map((x) => x.name).includes(args.sortField)) {
+          throw new GraphQLError(`Cannot sort by ${args.sortField} field`);
+        }
+      }
+
       const ability: AppAbility = context.user.ability;
       const abilityFilters = ApiConfiguration.find(
         accessibleBy(ability, 'read').ApiConfiguration
       ).getFilter();
-      const filters: any[] = [abilityFilters];
+      const queryFilters = getFilter(args.filter, FILTER_FIELDS);
+      const filters: any[] = [queryFilters, abilityFilters];
+
       const afterCursor = args.afterCursor;
+
+      // We only use one sort field
+      const sortField = SORT_FIELDS[0];
+      const sortOrder = args.sortOrder || 'asc';
+
       const cursorFilters = afterCursor
-        ? {
-            _id: {
-              $gt: decodeCursor(afterCursor),
-            },
-          }
+        ? sortField.cursorFilter(afterCursor, sortOrder)
         : {};
 
       let items: any[] = await ApiConfiguration.find({
         $and: [cursorFilters, ...filters],
       })
-        .sort({ _id: 1 })
+        .sort(sortField.sort(sortOrder))
         .limit(first + 1);
 
       const hasNextPage = items.length > first;
@@ -62,7 +115,7 @@ export default {
         items = items.slice(0, items.length - 1);
       }
       const edges = items.map((r) => ({
-        cursor: encodeCursor(r.id.toString()),
+        cursor: encodeCursor(sortField.cursorId(r)),
         node: r,
       }));
       return {
