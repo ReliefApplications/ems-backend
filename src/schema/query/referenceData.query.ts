30d65f04c645ffc5eb4d87ccc96c10fc1d0de298
Guilherme Soares Correa <55603259+guisoares1@users.noreply.github.com>
2023-12-12 19:17:32 -0300

    feat: ContextFilter applied  to referenceData.

--- src/schema/query/referenceData.query.ts
+++ src/schema/query/referenceData.query.ts
@@ -5,11 +5,51 @@ import { logger } from '@services/logger.service';
 import { graphQLAuthCheck } from '@schema/shared';
 import { Types } from 'mongoose';
 import { Context } from '@server/apollo/context';
+import GraphQLJSON from 'graphql-type-json';
+import { CompositeFilterDescriptor } from '@const/compositeFilter';
 
 /** Arguments for the referenceData query */
 type ReferenceDataArgs = {
   id: string | Types.ObjectId;
+  contextFilters?: CompositeFilterDescriptor;
 };
+
+/**
+ * Transforms query filter into mongo filter.
+ *
+ * @param filter filter to transform to mongo filter.
+ * @returns Mongo filter.
+ */
+async function buildQuery(filter, doc) {
+  // Em seguida, crie um conjunto dos nomes dos campos para facilitar a verificação
+  const fieldNames = new Set(doc.fields.map((field) => field.name));
+
+  if (filter.logic && filter.filters) {
+    const mongoFilters = await Promise.all(
+      filter.filters.map((subFilter) => {
+        console.log(subFilter); // remove this line
+        if (subFilter.logic && subFilter.filters) {
+          return buildQuery(subFilter, doc);
+        } else {
+          if (fieldNames.has(subFilter.field)) {
+            const obj = {};
+            if (subFilter.operator) {
+              obj[`data.${subFilter.field}`] = {
+                ['$' + subFilter.operator]: subFilter.value,
+              };
+            } else {
+              obj[`data.${subFilter.field}`] = subFilter.value;
+            }
+            return obj;
+          } else {
+            return;
+          }
+        }
+      })
+    );
+    return { ['$' + filter.logic]: mongoFilters };
+  }
+}
 
 /**
  * Return Reference Data from id if available for the logged user.
@@ -19,11 +59,21 @@ export default {
   type: ReferenceDataType,
   args: {
     id: { type: new GraphQLNonNull(GraphQLID) },
+    contextFilters: { type: GraphQLJSON },
   },
   async resolve(parent, args: ReferenceDataArgs, context: Context) {
     graphQLAuthCheck(context);
     try {
-      return await ReferenceData.findById(args.id);
+      const query: any = { _id: args.id }; // Pesquisa por ID sempre
+
+      if (args.contextFilters) {
+        const doc = await ReferenceData.findById(args.id);
+        const filterQuery = await buildQuery(args.contextFilters, doc);
+        query.$and = [query, filterQuery];
+      }
+
+      //console.log(query, args.contextFilters);
+      return await ReferenceData.find(query);
     } catch (err) {
       logger.error(err.message, { stack: err.stack });
       if (err instanceof GraphQLError) {
