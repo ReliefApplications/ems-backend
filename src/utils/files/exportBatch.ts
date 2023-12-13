ed227e63be76f49a2aba3f6b1bc68dea9e3cc303
RenzoPrats <renzoprats@icloud.com>
2023-12-13 09:47:59 -0300

    fix: reverted changes

--- src/utils/files/exportBatch.ts
+++ src/utils/files/exportBatch.ts
@@ -113,6 +113,9 @@ const getTotalCount = (
       headers: {
         Authorization: req.headers.authorization,
         'Content-Type': 'application/json',
+        ...(req.headers.accesstoken && {
+          accesstoken: req.headers.accesstoken,
+        }),
       },
       data: {
         query: totalCountQuery,
@@ -143,20 +146,32 @@ const getFlatColumns = (columns: any[]) => {
   let index = -1;
   return columns.reduce((acc, value) => {
     if (value.subColumns) {
-      return acc.concat(
-        value.subColumns.map((x) => {
-          index += 1;
-          return {
-            name: value.name,
-            title: value.title || value.name,
-            subName: x.name,
-            subTitle: x.title || x.name,
-            field: value.field,
-            subField: x.field,
-            index,
-          };
-        })
-      );
+      // Create nested headers
+      if (value.subColumns.length > 0) {
+        return acc.concat(
+          value.subColumns.map((x) => {
+            index += 1;
+            return {
+              name: value.name,
+              title: value.title || value.name,
+              subName: x.name,
+              subTitle: x.title || x.name,
+              field: value.field,
+              subField: x.field,
+              index,
+            };
+          })
+        );
+      } else {
+        // Create a single column as we see in the grid
+        index += 1;
+        return acc.concat({
+          name: value.name,
+          title: value.title || value.name,
+          field: value.field,
+          index,
+        });
+      }
     } else {
       index += 1;
       return acc.concat({
@@ -185,6 +200,9 @@ const getColumns = (req: any, params: ExportBatchParams): Promise<any[]> => {
       headers: {
         Authorization: req.headers.authorization,
         'Content-Type': 'application/json',
+        ...(req.headers.accesstoken && {
+          accesstoken: req.headers.accesstoken,
+        }),
       },
       data: {
         query: metaQuery,
@@ -194,23 +212,24 @@ const getColumns = (req: any, params: ExportBatchParams): Promise<any[]> => {
         if (Object.prototype.hasOwnProperty.call(data.data, field)) {
           const meta = data.data[field];
           const rawColumns = getColumnsFromMeta(meta, params.fields);
-          const columns = rawColumns.filter((x) =>
-            params.fields.find((f) => f.name === x.name)
-          );
           // Edits the column to match with the fields
-          columns.forEach((x) => {
-            const queryField = params.fields.find((f) => f.name === x.name);
-            x.title = queryField.title;
-            if (x.subColumns) {
-              x.subColumns.forEach((f) => {
-                const subQueryField = queryField.subFields.find(
-                  (z) => z.name === `${x.name}.${f.name}`
-                );
-                f.title = subQueryField.title;
-              });
+          rawColumns.forEach((column) => {
+            const queryField = params.fields.find(
+              (f) => f.name.toLowerCase() === column.name.toLowerCase()
+            );
+            column.title = queryField.title;
+            if (column.subColumns) {
+              if ((queryField.subFields || []).length > 0) {
+                column.subColumns.forEach((f) => {
+                  const subQueryField = queryField.subFields.find(
+                    (z) => z.name === `${column.name}.${f.name}`
+                  );
+                  f.title = subQueryField.title;
+                });
+              }
             }
           });
-          resolve(columns);
+          resolve(rawColumns);
         }
       }
     });
@@ -303,6 +322,9 @@ const getRowsXlsx = async (
         headers: {
           Authorization: req.headers.authorization,
           'Content-Type': 'application/json',
+          ...(req.headers.accesstoken && {
+            accesstoken: req.headers.accesstoken,
+          }),
         },
         data: {
           query,
@@ -374,6 +396,9 @@ const getRowsCsv = async (
       headers: {
         Authorization: req.headers.authorization,
         'Content-Type': 'application/json',
+        ...(req.headers.accesstoken && {
+          accesstoken: req.headers.accesstoken,
+        }),
       },
       data: {
         query,
