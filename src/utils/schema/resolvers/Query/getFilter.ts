4cf0a71a1d814eb86ef105be0e1162ecae20cdc3
estelafs <estelafersil@gmail.com>
2024-01-22 11:51:44 -0300

    set modifiedAt and createdAt filter as datetime

--- src/utils/schema/resolvers/Query/getFilter.ts
+++ src/utils/schema/resolvers/Query/getFilter.ts
@@ -16,11 +16,11 @@ const DEFAULT_FIELDS = [
   },
   {
     name: 'createdAt',
-    type: 'date',
+    type: 'datetime',
   },
   {
     name: 'modifiedAt',
-    type: 'date',
+    type: 'datetime',
   },
   {
     name: 'incrementalId',
