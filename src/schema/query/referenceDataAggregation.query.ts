7d6037f95ce37c594a09b5608d2321799b87347b
MwanPygmay <matt.brunot@laposte.net>
2024-02-06 12:54:32 +0100

    add support for aggregation on preview

--- src/schema/query/referenceDataAggregation.query.ts
+++ src/schema/query/referenceDataAggregation.query.ts
@@ -1,4 +1,5 @@
 import {
+  GraphQLBoolean,
   GraphQLError,
   GraphQLID,
   GraphQLInt,
@@ -343,6 +344,7 @@ type ReferenceDataAggregationArgs = {
   sortOrder?: string;
   contextFilters?: CompositeFilterDescriptor;
   graphQLVariables: any;
+  aggregationPreview?: boolean;
 };
 
 /**
@@ -364,6 +366,7 @@ export default {
     sortOrder: { type: GraphQLString },
     sortField: { type: GraphQLString },
     at: { type: GraphQLDate },
+    aggregationPreview: { type: GraphQLBoolean },
   },
   async resolve(parent, args: ReferenceDataAggregationArgs, context) {
     // Authentication check
@@ -388,7 +391,13 @@ export default {
       );
 
       // Check if resource exists and aggregation exists
-      if (!(referenceData && aggregation && referenceData.data)) {
+      if (
+        !(
+          referenceData &&
+          (aggregation || args.aggregationPreview) &&
+          referenceData.data
+        )
+      ) {
         throw new GraphQLError(context.i18next.t('common.errors.dataNotFound'));
       }
       // sourceFields and pipeline from args have priority over current aggregation ones
