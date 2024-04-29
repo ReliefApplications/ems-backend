ba18cb774710e7bd32edffe20eb0e58cabdcd894
Natasha Coia <natasha.c@adappt.ai>
2024-04-29 14:36:05 +0100

    added EmailNotification permissions to global admin role

--- src/security/defineUserAbility.ts
+++ src/security/defineUserAbility.ts
@@ -179,6 +179,7 @@ export default function defineUserAbility(user: User | Client): AppAbility {
         'Template',
         'DistributionList',
         'CustomNotification',
+        'EmailNotification',
       ]
     );
   } else {
