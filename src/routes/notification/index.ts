499030576293cf0b2c8ea8a2736759570370560f
Morgan Gowans <morgan.g@adappt.ai>
2024-05-01 17:08:59 +0100

    Fetch user and owner values for sent email

--- src/routes/notification/index.ts
+++ src/routes/notification/index.ts
@@ -22,6 +22,8 @@ import {
 } from '@utils/notification/htmlBuilder';
 import mongoose from 'mongoose';
 import { mergeArrayOfObjects } from '@schema/types/dataSet.type';
+import { getOwnerOptions, getUsersOptions } from '@utils/form/metadata.helper';
+import _ from 'lodash';
 
 /**
  *
@@ -249,13 +251,51 @@ router.post('/send-email/:configId', async (req, res) => {
         for (const obj of tempRecords) {
           const data = obj?.data;
           for (const [key, value] of Object.entries(data)) {
-            if (mongoose.isValidObjectId(value) && typeof value === 'string') {
+            const userField = fields.find((x) => {
+              return x.type === 'users' && x.name === key;
+            });
+            const ownerField = fields.find((x) => {
+              return x.type === 'owner' && x.name === key;
+            });
+            if (mongoose.isValidObjectId(value) && typeof value == 'string') {
               const project = mergeArrayOfObjects(nestedFields[key]) ?? {};
               Object.assign(project, { _id: 0 });
               const record = await Record.findById(value, project);
               data[key] = record;
+              console.log('HIT');
+            }
+            if (value instanceof Array) {
+              if (userField) {
+                const options = await getUsersOptions(userField.applications);
+                const users = value.map((user: any) => {
+                  const values = options.filter((opt) => {
+                    return opt.value.toString() === user;
+                  });
+                  return _.map(values, 'text');
+                });
+
+                const usersJoined = users.join(', ');
+                data[key] =
+                  usersJoined.length > 0 ? usersJoined : value.join(', ');
+              }
+              if (ownerField) {
+                const ownerOptions = await getOwnerOptions(
+                  ownerField.applications
+                );
+                const owners = value.map((owner: any) => {
+                  const values = ownerOptions.filter((opt) => {
+                    return opt.value.toString() === owner;
+                  });
+                  return _.map(values, 'text');
+                });
+
+                const ownersJoined = owners.join(', ');
+                data[key] =
+                  ownersJoined.length > 0 ? ownersJoined : value.join(', ');
+              }
             }
           }
+
           Object.assign(obj, data);
         }
 
@@ -408,13 +448,13 @@ router.post('/send-individual-email/:configId', async (req, res) => {
     const datasets = config.get('dataSets');
     let emailElement = parse(`<!DOCTYPE HTML>
     <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-    
+
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
         <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
         <title>EMSPOC - Email Alert</title>
     </head>
-    
+
     <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="width: 100%; background-color: #ffffff; margin: 0; padding: 0; -webkit-font-smoothing: antialiased;">
         <table border="0" width="100%" cellpadding="0" cellspacing="0">
             <tr>
@@ -424,14 +464,14 @@ router.post('/send-individual-email/:configId', async (req, res) => {
                 <td width="100%" align="center" valign="top" bgcolor="#ffffff">
                     <!----------   main content----------->
                     <table id="mainTable" width="800" style="border: 3px solid #00205c; margin: 0 auto;" cellpadding="0" cellspacing="0" bgcolor="#fff">
-                        
+
                     </table>
                     <!----------   end main content----------->
                 </td>
             </tr>
         </table>
     </body>
-    
+
     </html>`);
 
     const mainTableElement = emailElement.getElementById('mainTable');
