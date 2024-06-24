684a0e1ab2dfdf5b58cdeeacba829fdc980fdd29
Estela Silva <estelafersil@gmail.com>
2024-06-24 14:08:39 -0300

    added calculate questions, added mapping for iif function & refactored properties constructor

--- src/utils/form/extractKoboFields.ts
+++ src/utils/form/extractKoboFields.ts
@@ -23,6 +23,7 @@ const AVAILABLE_TYPES = [
   'audio',
   'video',
   'geoshape',
+  'calculate',
 ];
 
 /**
@@ -68,6 +69,8 @@ const mapKoboExpression = (koboExpression: string, questionName?: string) => {
     /\$\{(\w+)\} != '(.*?)'/g,
     "{$1} <> '$2'"
   );
+  // Replace if with iif
+  koboExpression = koboExpression.replace(/if\(/g, 'iif(');
   // Date values
   koboExpression = koboExpression.replace(/date\('(.*?)'\)/g, "'$1'");
   // Replace any remaining ${variable} to {variable}
@@ -102,6 +105,31 @@ const validators = (question: any) => {
 };
 
 /**
+ * Extract from a Kobo question the common properties in a object for a SurveyJS question
+ *
+ * @param question Kobo question object
+ * @param type type of the questions
+ * @param title optional title
+ * @returns the common properties in a object for a SurveyJS question extracted from the Kobo question
+ */
+const commonProperties = (question: any, type: string, title?: string) => {
+  return {
+    type,
+    name: question.$autoname.toLowerCase(),
+    title: title ?? question.label[0],
+    valueName: question.$autoname.toLowerCase(),
+    isRequired: question.required,
+    ...(question.hint && { description: question.hint[0] }),
+    // TODO: make sure that the defaultValue works for all type of questions. Works for sure for the questions type: integer, text, decimal, range.
+    ...(question.default && { defaultValue: question.default }),
+    ...(question.relevant && {
+      visibleIf: mapKoboExpression(question.relevant),
+    }),
+    ...(question.constraint && validators(question)),
+  };
+};
+
+/**
  * Extract kobo form fields and convert to oort fields
  *
  * @param survey survey structure
@@ -129,18 +157,8 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
       switch (question.type) {
         case 'decimal': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            isRequired: question.required,
+            ...commonProperties(question, 'text'),
             inputType: 'number',
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.default && { defaultValue: question.default }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
@@ -148,16 +166,7 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
         case 'geoshape':
         case 'geopoint': {
           const newQuestion = {
-            type: 'geospatial',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            isRequired: question.required,
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
+            ...commonProperties(question, 'geospatial'),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
@@ -165,15 +174,13 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
         case 'select_one':
         case 'select_multiple': {
           const newQuestion = {
-            type:
-              question.type === 'select_multiple' ? 'checkbox' : 'radiogroup',
+            ...commonProperties(
+              question,
+              question.type === 'select_multiple' ? 'checkbox' : 'radiogroup'
+            ),
             ...(question.type === 'select_multiple' && {
               showSelectAllItem: true,
             }),
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            isRequired: question.required,
             choices: choices
               .filter(
                 (choice) => question.select_from_list_name === choice.list_name
@@ -182,47 +189,25 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
                 value: choice.$autovalue,
                 text: choice.label[0],
               })),
-            ...(question.hint && { description: question.hint[0] }),
             ...(question.parameters &&
               question.parameters.split('randomize=')[1]?.includes('true') && {
                 choicesOrder: 'random',
               }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'date': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            isRequired: question.required,
-            valueName: question.$autoname.toLowerCase(),
+            ...commonProperties(question, 'text'),
             inputType: 'date',
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'note': {
           const newQuestion = {
-            type: 'expression',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
+            ...commonProperties(question, 'expression'),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
@@ -233,21 +218,13 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
         }
         case 'score__row': {
           const newQuestion = {
-            type: 'radiogroup',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            isRequired: question.required,
+            ...commonProperties(question, 'radiogroup'),
             choices: choices
               .filter((choice) => scoreChoiceId === choice.list_name)
               .map((choice) => ({
                 value: choice.$autovalue,
                 text: choice.label[0],
               })),
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
             // This question does not have Validation Criteria settings (validators property)
           };
           questions.pages[0].elements.push(newQuestion);
@@ -259,21 +236,13 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
         }
         case 'rank__level': {
           const newQuestion = {
-            type: 'dropdown',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            isRequired: question.required,
+            ...commonProperties(question, 'dropdown'),
             choices: choices
               .filter((choice) => rankChoiceId === choice.list_name)
               .map((choice) => ({
                 value: choice.$autovalue,
                 text: choice.label[0],
               })),
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
             // This question does not have Validation Criteria settings
           };
           questions.pages[0].elements.push(newQuestion);
@@ -281,34 +250,15 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
         }
         case 'text': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            isRequired: question.required,
-            valueName: question.$autoname.toLowerCase(),
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.default && { defaultValue: question.default }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
+            ...commonProperties(question, 'text'),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'time': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            isRequired: question.required,
-            valueName: question.$autoname.toLowerCase(),
+            ...commonProperties(question, 'text'),
             inputType: 'time',
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
@@ -318,86 +268,51 @@ export const extractKoboFields = (survey: any, title: string, choices: any) => {
         case 'image':
         case 'file': {
           const newQuestion = {
-            type: 'file',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            isRequired: question.required,
-            valueName: question.$autoname.toLowerCase(),
+            ...commonProperties(question, 'file'),
             storeDataAsText: false,
             maxSize: 7340032,
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'integer': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            isRequired: question.required,
-            valueName: question.$autoname.toLowerCase(),
+            ...commonProperties(question, 'text'),
             inputType: 'number',
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.default && { defaultValue: question.default }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'datetime': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            isRequired: question.required,
-            valueName: question.$autoname.toLowerCase(),
+            ...commonProperties(question, 'text'),
             inputType: 'datetime-local',
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'acknowledge': {
           const newQuestion = {
-            type: 'boolean',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
+            ...commonProperties(question, 'boolean'),
           };
           questions.pages[0].elements.push(newQuestion);
           break;
         }
         case 'range': {
           const newQuestion = {
-            type: 'text',
-            name: question.$autoname.toLowerCase(),
-            title: question.label[0],
-            valueName: question.$autoname.toLowerCase(),
+            ...commonProperties(question, 'text'),
             inputType: 'range',
             step: question.parameters.split('step=')[1],
-            ...(question.hint && { description: question.hint[0] }),
-            ...(question.default && { defaultValue: question.default }),
-            ...(question.relevant && {
-              visibleIf: mapKoboExpression(question.relevant),
-            }),
-            ...(question.constraint && validators(question)),
+          };
+          questions.pages[0].elements.push(newQuestion);
+          break;
+        }
+        case 'calculate': {
+          const newQuestion = {
+            ...commonProperties(question, 'expression', question.calculation),
+            visible: false, // They are not displayed in the Kobo form, so make it invisible by default for the SurveyJS
+            expression: mapKoboExpression(question.calculation),
+            // This question does not have hint (description)
           };
           questions.pages[0].elements.push(newQuestion);
           break;
