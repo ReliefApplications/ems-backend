79ca8864d166e5e1076343b375abf43f4485b92a
AjithKumar <ajithkumar.p@adappt.ai>
2024-08-13 16:11:27 +0530

    quick action send email header issue fixed

--- src/utils/notification/htmlBuilder.ts
+++ src/utils/notification/htmlBuilder.ts
@@ -212,7 +212,9 @@ export const buildTable = (
       table += '<tr bgcolor="#00205c">';
       dataset.columns.forEach((field) => {
         table += `<th align="left" style="color: #fff; font-size: 14px; font-family: 'Roboto', Arial, sans-serif; padding-left: 10px">${titleCase(
-          replaceUnderscores(`${field.label ?? field}`)
+          replaceUnderscores(
+            `${field.label ?? (typeof field === 'string' ? field : field.name)}`
+          )
         )}</th>`;
       });
 
