1826c956c4a2da367d331fdcf4319f579f8ffbc4
Antoine Hurard <antoine.reliefapps@gmail.com>
2024-08-30 08:54:17 -0300

    update script

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo -e "Stopping docker containers..."
-CMD="cd $REMOTE_PATH && echo $SSH_PASS | sudo -S docker-compose pull api"
+CMD="cd $REMOTE_PATH && echo $SSH_PASS | sudo -S docker compose pull api"
 ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "$CMD"
 
 echo -e "Stopping docker containers..."
-CMD="cd $REMOTE_PATH && echo $SSH_PASS | sudo -S docker-compose up -d --no-deps api"
+CMD="cd $REMOTE_PATH && echo $SSH_PASS | sudo -S docker compose up -d --no-deps api"
 ssh -oStrictHostKeyChecking=no -o PubkeyAuthentication=yes $CONNECTION "$CMD"
 
 echo -e "Deployed!"
