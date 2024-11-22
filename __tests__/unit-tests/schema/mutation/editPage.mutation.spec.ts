58d8d349269d53a0359ffb285b894ffce1e67f08
Tai Kamilla <taikamilla@gmail.com>
2024-11-22 09:38:16 +0100

    Add mocks for permissions in editPage tests
    - Mocked the `extendAbilityForPage` function to control permission checks.
    - Updated tests to reset mock implementations as needed.
    - Uncommented and fixed the test for updating a page with valid arguments.
    - Adjusted error handling tests to reflect new scenarios.

--- __tests__/unit-tests/schema/mutation/editPage.mutation.spec.ts
+++ __tests__/unit-tests/schema/mutation/editPage.mutation.spec.ts
@@ -21,6 +21,15 @@ type EditPageArgs = {
   visible: boolean;
 };
 
+// Mock the extendAbilityForPage function
+jest.mock('@security/extendAbilityForPage', () => ({
+  __esModule: true,
+  default: jest.fn().mockResolvedValue({
+    can: jest.fn().mockReturnValue(true),
+    cannot: jest.fn().mockReturnValue(false),
+  }),
+}));
+
 describe('editPage Resolver', () => {
   let context: Context;
   let args: EditPageArgs;
@@ -76,6 +85,12 @@ describe('editPage Resolver', () => {
       },
       visible: true,
     };
+
+    // Reset the mock implementation extendAbilityForPage
+    (extendAbilityForPage as jest.Mock).mockResolvedValue({
+      can: jest.fn().mockReturnValue(true),
+      cannot: jest.fn().mockReturnValue(false),
+    });
   });
   describe('Authentication and Authorization', () => {
     it('should throw an error if the user is not authenticated', async () => {
@@ -88,6 +103,10 @@ describe('editPage Resolver', () => {
     });
 
     it('should throw an error if the user does not have permission to update the page', async () => {
+      (extendAbilityForPage as jest.Mock).mockResolvedValue({
+        can: jest.fn().mockReturnValue(false),
+        cannot: jest.fn().mockReturnValue(true),
+      });
       const result = editPage.resolve(null, args, context);
       await expect(result).rejects.toThrow(GraphQLError);
       expect(context.i18next.t).toHaveBeenCalledWith(
@@ -116,20 +135,20 @@ describe('editPage Resolver', () => {
   });
 
   describe('Page Update Logic', () => {
-    // it('should update the page if valid arguments are provided', async () => {
-    //   const updatedArgs = {
-    //     ...args,
-    //     name: 'Updated Page Name',
-    //     visible: false,
-    //     icon: 'new-icon',
-    //     permissions: { canSee: [], canUpdate: [], canDelete: [] },
-    //   };
-    //   const result = await editPage.resolve(null, updatedArgs, context);
-    //   expect(result.name).toBe(updatedArgs.name);
-    //   expect(result.visible).toBe(updatedArgs.visible);
-    //   expect(result.icon).toBe(updatedArgs.icon);
-    //   expect(result.permissions).toEqual(updatedArgs.permissions);
-    // });
+    it('should update the page if valid arguments are provided', async () => {
+      const updatedArgs = {
+        ...args,
+        name: 'Updated Page Name',
+        visible: false,
+        icon: 'new-icon',
+        permissions: { canSee: [], canUpdate: [], canDelete: [] },
+      };
+      const result = await editPage.resolve(null, updatedArgs, context);
+      expect(result.name).toBe(updatedArgs.name);
+      expect(result.visible).toBe(updatedArgs.visible);
+      expect(result.icon).toBe(updatedArgs.icon);
+      expect(result.permissions).toEqual(updatedArgs.permissions);
+    });
   });
 
   describe('Permission Update Logic', () => {
@@ -148,25 +167,7 @@ describe('editPage Resolver', () => {
 
   describe('Content Update Logic', () => {
     it('should update the content and properties for workflow pages', async () => {
-      // // create a workflow
-      // const workflowPage = await addPage.resolve(
-      //   null,
-      //   {
-      //     application: new Types.ObjectId().toHexString(),
-      //     type: 'workflow',
-      //     content: new Types.ObjectId().toHexString(),
-      //   },
-      //   context
-      // );
-      // const updatedArgs = {
-      //   ...args,
-      //   id: workflowPage.id,
-      //   name: 'Updated Workflow Page',
-      //   permissions: { canSee: [], canUpdate: [], canDelete: [] },
-      // };
-      // const result = await editPage.resolve(null, updatedArgs, context);
-      // expect(result.name).toBe(updatedArgs.name);
-      // expect(result.permissions).toEqual(updatedArgs.permissions);
+      // Test implementation
     });
 
     it('should update the content name and properties for dashboard pages', async () => {
@@ -189,15 +190,10 @@ describe('editPage Resolver', () => {
 
     it('should return a translated error message if a GraphQLError is thrown', async () => {
       jest
-        .spyOn(Page, 'findByIdAndUpdate')
-        .mockRejectedValue(
-          new GraphQLError('common.errors.permissionNotGranted')
-        );
+        .spyOn(Page, 'findById')
+        .mockRejectedValue(new GraphQLError('common.errors.dataNotFound'));
       const result = editPage.resolve(null, args, context);
-      await expect(result).rejects.toThrow(GraphQLError);
-      expect(context.i18next.t).toHaveBeenCalledWith(
-        'common.errors.permissionNotGranted'
-      );
+      expect(result).rejects.toThrow('common.errors.dataNotFound');
     });
 
     it('should throw an error if permissions update logic fails', async () => {
