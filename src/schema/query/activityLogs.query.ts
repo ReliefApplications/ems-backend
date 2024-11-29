b53142f935fb95ec3067c3631b76b3a10abf48d8
Tai Kamilla <taikamilla@gmail.com>
2024-11-29 18:19:18 +0100

    Add filtering to activity logs query
    - Introduced arguments for userId and applicationId.
    - Updated resolve function to handle these new args.
    - Added query conditions based on provided arguments.
    - Included console logs for debugging purposes.

--- src/schema/query/activityLogs.query.ts
+++ src/schema/query/activityLogs.query.ts
@@ -1,6 +1,6 @@
 import { ActivityLog } from '@models';
 import { ActivityLogType } from '../types';
-import { GraphQLError, GraphQLList } from 'graphql';
+import { GraphQLError, GraphQLList, GraphQLString } from 'graphql';
 import { logger } from '@services/logger.service';
 
 /**
@@ -8,9 +8,23 @@ import { logger } from '@services/logger.service';
  */
 export default {
   type: new GraphQLList(ActivityLogType),
-  async resolve() {
+  args: {
+    userId: { type: GraphQLString },
+    applicationId: { type: GraphQLString },
+  },
+  async resolve(_, args) {
     try {
-      const activities = await ActivityLog.find();
+      const { userId, applicationId } = args;
+
+      const queryConditions: { [key: string]: string | undefined } = {};
+      if (userId) queryConditions.userId = userId;
+      if (applicationId) queryConditions.applicationId = applicationId;
+      console.log('queryConditions', queryConditions);
+      console.log('args', args);
+      console.log('userId', userId);
+      console.log('applicationId', applicationId);
+
+      const activities = await ActivityLog.find(queryConditions);
       return activities;
     } catch (err) {
       logger.error(err.message, { stack: err.stack });
