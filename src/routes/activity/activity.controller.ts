9142d1832729d587a8a8badfa5af1451bf0c9903
Antoine Hurard <antoine@reliefapplications.org>
2024-12-13 12:05:03 +0100

    refactor: use controller & service instead of index file for activity

--- src/routes/activity/activity.controller.ts
+++ src/routes/activity/activity.controller.ts
@@ -4,6 +4,7 @@ import { ActivityService } from './activity.service';
 import ApiError from 'abstractions/api-error';
 import BaseController from 'abstractions/base.controller';
 import { RouteDefinition } from 'types/route-definition';
+import { isNil } from 'lodash';
 
 /**
  * Activity controller.
@@ -23,12 +24,33 @@ export default class ActivityController extends BaseController {
     this.activity = new ActivityService();
   }
 
+  /** @returns List of routes & handlers */
   public routes(): RouteDefinition[] {
     return [
       {
         path: '/',
         method: 'post',
         handler: this.createActivity.bind(this),
+      },
+      {
+        path: '/',
+        method: 'get',
+        handler: this.getActivities.bind(this),
+      },
+      {
+        path: '/group-by-url',
+        method: 'get',
+        handler: this.groupByUrl.bind(this),
+      },
+      {
+        path: '/group-by-user',
+        method: 'get',
+        handler: this.groupByUser.bind(this),
+      },
+      {
+        path: '/download',
+        method: 'post',
+        handler: this.downloadList.bind(this),
       },
     ];
   }
@@ -58,6 +80,180 @@ export default class ActivityController extends BaseController {
       });
       res.locals.data = activity;
       this.send(res, StatusCodes.CREATED);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  /**
+   * Get list of activities
+   *
+   * @param req Express request
+   * @param res Express response
+   * @param next Express next function
+   */
+  public async getActivities(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const {
+        skip,
+        take,
+        sortField,
+        sortOrder,
+        filter,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        application_id,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        user_id,
+      } = req.query;
+      const aggregation = await this.activity.list({
+        ...(application_id && { applicationId: application_id as string }),
+        ...(user_id && { userId: user_id as string }),
+        ...(!isNil(skip) && { skip: Number(skip) }),
+        ...(!isNil(take) && { take: Number(take) }),
+        sortField: sortField as string,
+        sortOrder: sortOrder as string,
+        ...(filter && { filter: JSON.parse(filter as string) }),
+      });
+      res.locals.data = {
+        data: aggregation.items,
+        skip: aggregation.skip,
+        take: aggregation.take,
+        total: aggregation.total,
+      };
+      this.send(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  /**
+   * Get Group activities by url
+   *
+   * @param req Express request
+   * @param res Express response
+   * @param next Express next function
+   */
+  public async groupByUrl(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const {
+        skip,
+        take,
+        sortField,
+        sortOrder,
+        filter,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        application_id,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        user_id,
+      } = req.query;
+      const aggregation = await this.activity.groupByUrl({
+        ...(application_id && { applicationId: application_id as string }),
+        ...(user_id && { userId: user_id as string }),
+        ...(!isNil(skip) && { skip: Number(skip) }),
+        ...(!isNil(take) && { take: Number(take) }),
+        sortField: sortField as string,
+        sortOrder: sortOrder as string,
+        ...(filter && { filter: JSON.parse(filter as string) }),
+      });
+      res.locals.data = {
+        data: aggregation.items,
+        skip: aggregation.skip,
+        take: aggregation.take,
+        total: aggregation.total,
+      };
+      this.send(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  /**
+   * Get group activities by user
+   *
+   * @param req Express request
+   * @param res Express response
+   * @param next Express next function
+   */
+  public async groupByUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const {
+        skip,
+        take,
+        sortField,
+        sortOrder,
+        filter,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        application_id,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        user_id,
+      } = req.query;
+      const aggregation = await this.activity.groupByUser({
+        ...(application_id && { applicationId: application_id as string }),
+        ...(user_id && { userId: user_id as string }),
+        ...(!isNil(skip) && { skip: Number(skip) }),
+        ...(!isNil(take) && { take: Number(take) }),
+        sortField: sortField as string,
+        sortOrder: sortOrder as string,
+        ...(filter && { filter: JSON.parse(filter as string) }),
+      });
+      res.locals.data = {
+        data: aggregation.items,
+        skip: aggregation.skip,
+        take: aggregation.take,
+        total: aggregation.total,
+      };
+      this.send(res);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  /**
+   * Download list of activities
+   *
+   * @param req Express request
+   * @param res Express response
+   * @param next Express next function
+   */
+  public async downloadList(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const {
+        timeZone,
+        sortField,
+        sortOrder,
+        filter,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        applicationId,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        userId,
+      } = req.body;
+      const { fileName, file } = await this.activity.downloadList({
+        ...(timeZone && { timeZone: timeZone as string }),
+        ...(applicationId && { applicationId: applicationId as string }),
+        ...(userId && { userId: userId as string }),
+        sortField: sortField as string,
+        sortOrder: sortOrder as string,
+        ...(filter && { filter }),
+      });
+      res.locals.data = file;
+      res.attachment(fileName);
+      this.send(res);
     } catch (err) {
       next(err);
     }
--- src/routes/activity/activity.service.ts
+++ src/routes/activity/activity.service.ts
@@ -1,5 +1,11 @@
 import { ActivityLog } from '@models/activityLog.model';
 import { logger } from '@services/logger.service';
+import getFilter from '@utils/filter/getFilter';
+import { isNil } from 'lodash';
+import { Types } from 'mongoose';
+import { CompositeFilterDescriptor } from 'types';
+import config from 'config';
+import xlsBuilder from '@utils/files/xlsBuilder';
 
 /** Activity creation attributes interface */
 interface ActivityCreationAttributes {
@@ -7,10 +13,74 @@ interface ActivityCreationAttributes {
   eventType: string;
   metadata: any;
 }
+
+/** Activity query options */
+interface QueryOptions {
+  skip?: number;
+  take?: number;
+  sortField?: string;
+  sortOrder?: string;
+  filter?: CompositeFilterDescriptor;
+  userId?: string;
+  applicationId?: string;
+  timeZone?: string;
+}
+
+/** Date format options */
+const dateFormatOptions: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  hourCycle: 'h23', // Ensures 24-hour format
+};
+
+/** Available filter fields */
+const FILTER_FIELDS: { name: string; type: string }[] = [
+  {
+    name: 'createdAt',
+    type: 'date',
+  },
+  {
+    name: 'username',
+    type: 'text',
+  },
+  ...((config.get('user.attributes.list') || []) as any[]).map((x) => ({
+    name: `attributes.${x.value}`,
+    type: 'text',
+  })),
+  {
+    name: 'metadata.title',
+    type: 'text',
+  },
+];
+
+/** Get attributes from configuration */
+const attributes: any[] = config.get('user.attributes.list') || [];
+
 /**
  * Activity service
  */
 export class ActivityService {
+  /** Filename for list export */
+  private listExportFileName = 'activities.xlsx';
+
+  /** Columns for list export */
+  private listExportColumns = [
+    { name: 'timestamp', title: 'Timestamp', field: 'timestamp' },
+    { name: 'userId', title: 'User ID', field: 'userId' },
+    { name: 'username', title: 'username', field: 'username' },
+    ...attributes.map((x) => {
+      return {
+        name: x.text,
+        title: x.text,
+        field: `attributes.${x.value}`,
+      };
+    }),
+    { name: 'page', title: 'Page', field: 'page' },
+  ];
+
   /**
    * Create and save a new activity
    *
@@ -34,9 +104,409 @@ export class ActivityService {
     }
   }
 
-  async getAll() {}
+  /**
+   * Get list of activities
+   *
+   * @param query Query options
+   * @returns List of activities & pagination details
+   */
+  async list(query: QueryOptions) {
+    try {
+      const skip = Number(query.skip || 0);
+      const take = Number(query.take || 10);
+      const sortField = (query.sortField || 'createdAt') as string;
+      const sortOrder = (query.sortOrder || 'desc') as string;
+      const filters: any[] = [
+        ...(query.userId
+          ? [{ userId: new Types.ObjectId(query.userId as string) }]
+          : []),
+        ...(query.applicationId
+          ? [{ 'metadata.applicationId': query.applicationId }]
+          : []),
+      ];
 
-  async groupByUrl() {}
+      if (!isNil(query.filter)) {
+        const queryFilters = getFilter(query.filter, FILTER_FIELDS);
+        filters.push(queryFilters);
+      }
+      const aggregation = await this.listAggregation(
+        sortField,
+        sortOrder,
+        filters
+      )
+        .facet({
+          items: [
+            {
+              $skip: skip,
+            },
+            {
+              $limit: take,
+            },
+          ],
+          totalCount: [
+            {
+              $count: 'count',
+            },
+          ],
+        })
+        .project({
+          items: 1,
+          // So totalCount is 0 if no item found
+          totalCount: {
+            $ifNull: [{ $arrayElemAt: ['$totalCount.count', 0] }, 0],
+          },
+        });
+      return {
+        items: aggregation[0].items,
+        skip,
+        take,
+        total: aggregation[0].totalCount,
+      };
+    } catch (error) {
+      logger.error(error.message, { stack: error.stack });
+      throw error;
+    }
+  }
 
-  async groupByUser() {}
+  /**
+   * Group activities by url
+   *
+   * @param query Query options
+   * @returns Group of activities & pagination details
+   */
+  async groupByUrl(query: QueryOptions) {
+    try {
+      const skip = Number(query.skip || 0);
+      const take = Number(query.take || 10);
+      const sortField = (query.sortField || 'count') as string;
+      const sortOrder = (query.sortOrder || 'desc') as string;
+      const filters: any[] = [
+        ...(query.userId
+          ? [{ userId: new Types.ObjectId(query.userId as string) }]
+          : []),
+        ...(query.applicationId
+          ? [{ 'metadata.applicationId': query.applicationId }]
+          : []),
+      ];
+
+      if (!isNil(query.filter)) {
+        const queryFilters = getFilter(query.filter, FILTER_FIELDS);
+        filters.push(queryFilters);
+      }
+
+      const aggregation = await this.groupByUrlAggregation(
+        sortField,
+        sortOrder,
+        filters
+      )
+        .facet({
+          items: [
+            {
+              $skip: skip,
+            },
+            {
+              $limit: take,
+            },
+          ],
+          totalCount: [
+            {
+              $count: 'count',
+            },
+          ],
+        })
+        .project({
+          items: 1,
+          // So totalCount is 0 if no item found
+          totalCount: {
+            $ifNull: [{ $arrayElemAt: ['$totalCount.count', 0] }, 0],
+          },
+        });
+      return {
+        items: aggregation[0].items,
+        skip,
+        take,
+        total: aggregation[0].totalCount,
+      };
+    } catch (error) {
+      logger.error(error.message, { stack: error.stack });
+      throw error;
+    }
+  }
+
+  /**
+   * Group activities by user
+   *
+   * @param query Query options
+   * @returns Group of activities & pagination details
+   */
+  async groupByUser(query: QueryOptions) {
+    try {
+      const skip = Number(query.skip || 0);
+      const take = Number(query.take || 10);
+      const sortField = (query.sortField || 'count') as string;
+      const sortOrder = (query.sortOrder || 'desc') as string;
+      const filters: any[] = [
+        ...(query.userId
+          ? [{ userId: new Types.ObjectId(query.userId as string) }]
+          : []),
+        ...(query.applicationId
+          ? [{ 'metadata.applicationId': query.applicationId }]
+          : []),
+      ];
+
+      if (!isNil(query.filter)) {
+        const queryFilters = getFilter(query.filter, FILTER_FIELDS);
+        filters.push(queryFilters);
+      }
+
+      const aggregation = await this.groupByUserAggregation(
+        sortField,
+        sortOrder,
+        filters
+      )
+        .facet({
+          items: [
+            {
+              $skip: skip,
+            },
+            {
+              $limit: take,
+            },
+          ],
+          totalCount: [
+            {
+              $count: 'count',
+            },
+          ],
+        })
+        .project({
+          items: 1,
+          // So totalCount is 0 if no item found
+          totalCount: {
+            $ifNull: [{ $arrayElemAt: ['$totalCount.count', 0] }, 0],
+          },
+        });
+      return {
+        items: aggregation[0].items,
+        skip,
+        take,
+        total: aggregation[0].totalCount,
+      };
+    } catch (error) {
+      logger.error(error.message, { stack: error.stack });
+      throw error;
+    }
+  }
+
+  /**
+   * Download list of activities
+   *
+   * @param query Query options
+   * @returns File buffer
+   */
+  async downloadList(query: QueryOptions) {
+    try {
+      const timeZone: string = query.timeZone || 'UTC';
+      const sortField = (query.sortField || 'createdAt') as string;
+      const sortOrder = (query.sortOrder || 'desc') as string;
+      const filters: any[] = [
+        ...(query.userId
+          ? [{ userId: new Types.ObjectId(query.userId as string) }]
+          : []),
+        ...(query.applicationId
+          ? [{ 'metadata.applicationId': query.applicationId }]
+          : []),
+      ];
+
+      if (!isNil(query.filter)) {
+        const queryFilters = getFilter(query.filter, FILTER_FIELDS);
+        filters.push(queryFilters);
+      }
+
+      const aggregation = await this.listAggregation(
+        sortField,
+        sortOrder,
+        filters
+      );
+      const formattedData = aggregation.map((activity) => ({
+        timestamp: this.formatDate(activity.createdAt, timeZone),
+        userId: activity.userId?.toString(),
+        page: activity.metadata?.title || '',
+        username: activity.username,
+        attributes: activity.attributes,
+      }));
+      const file = await xlsBuilder(
+        this.listExportFileName,
+        this.listExportColumns,
+        formattedData
+      );
+      return { fileName: this.listExportFileName, file };
+    } catch (error) {
+      logger.error(error.message, { stack: error.stack });
+      throw error;
+    }
+  }
+
+  /**
+   * Format date
+   *
+   * @param date Date to format
+   * @param timeZone User timezone
+   * @returns formatted date
+   */
+  private formatDate(date: Date, timeZone: string) {
+    const formattedParts = new Intl.DateTimeFormat('en-GB', {
+      ...dateFormatOptions,
+      timeZone,
+    })
+      .formatToParts(date) // Breaks down the date into individual parts
+      .reduce(
+        (acc, part) => ({ ...acc, [part.type]: part.value }),
+        {} as Record<string, string>
+      );
+    return `${formattedParts.year}-${formattedParts.month}-${formattedParts.day} ${formattedParts.hour}:${formattedParts.minute}`;
+  }
+
+  /**
+   * List aggregation
+   *
+   * @param sortField Sort field
+   * @param sortOrder Sort order
+   * @param filters Filters
+   * @returns List activities aggregation
+   */
+  private listAggregation(
+    sortField: string,
+    sortOrder: string,
+    filters: any[]
+  ) {
+    return ActivityLog.aggregate([
+      // Only add filters if relevant
+      ...(filters.length > 0
+        ? [
+            {
+              $match: {
+                $and: [...filters],
+              },
+            },
+          ]
+        : []),
+      {
+        $project: {
+          _id: 1,
+          userId: 1,
+          username: 1,
+          metadata: {
+            $mergeObjects: [
+              '$metadata',
+              {
+                title: { $ifNull: ['$metadata.title', '$metadata.url'] },
+              },
+            ],
+          },
+          attributes: 1,
+          createdAt: 1,
+        },
+      },
+      {
+        $sort: { [sortField]: sortOrder === 'asc' ? 1 : -1 },
+      },
+    ]);
+  }
+
+  /**
+   * Group by url aggregation
+   *
+   * @param sortField Sort field
+   * @param sortOrder Sort order
+   * @param filters Filters
+   * @returns Group by url aggregation
+   */
+  private groupByUrlAggregation(
+    sortField: string,
+    sortOrder: string,
+    filters: any[]
+  ) {
+    return ActivityLog.aggregate([
+      // Only add filters if relevant
+      ...(filters.length > 0
+        ? [
+            {
+              $match: {
+                $and: [...filters],
+              },
+            },
+          ]
+        : []),
+      {
+        $group: {
+          _id: '$metadata.url',
+          count: { $sum: 1 },
+          title: { $last: '$metadata.title' },
+        },
+      },
+      {
+        $project: {
+          count: 1,
+          _id: 0,
+          'metadata.title': {
+            $ifNull: ['$title', '$_id'],
+          },
+        },
+      },
+      {
+        $sort: { [sortField]: sortOrder === 'asc' ? 1 : -1 },
+      },
+    ]);
+  }
+
+  /**
+   * Group by user aggregation
+   *
+   * @param sortField Sort field
+   * @param sortOrder Sort order
+   * @param filters Filters
+   * @returns Group by user aggregation
+   */
+  private groupByUserAggregation(
+    sortField: string,
+    sortOrder: string,
+    filters: any[]
+  ) {
+    return ActivityLog.aggregate([
+      // Only add filters if relevant
+      ...(filters.length > 0
+        ? [
+            {
+              $match: {
+                $and: [...filters],
+              },
+            },
+          ]
+        : []),
+      {
+        $group: {
+          _id: {
+            username: '$username',
+            attributes: {
+              $cond: [{ $not: ['$username'] }, '$attributes', null],
+            },
+          },
+          count: { $sum: 1 },
+          attributes: { $last: '$attributes' },
+        },
+      },
+      {
+        $project: {
+          username: '$_id.username',
+          count: 1,
+          attributes: 1,
+          _id: 0,
+        },
+      },
+      {
+        $sort: { [sortField]: sortOrder === 'asc' ? 1 : -1 },
+      },
+    ]);
+  }
 }
