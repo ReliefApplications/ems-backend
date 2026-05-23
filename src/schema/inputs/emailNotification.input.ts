023ec525d1f143db9d1f4213904dbf7ffd3d17ae
Morgan Gowans <morgan.g@adappt.ai>
2026-05-23 01:55:16 +0100

    Feat: Add csFilter to send seperate dataset for CS user filters
    (cherry picked from commit 86e9d4f0e00fb963ee63ab64e65b4efb171de727)

--- src/schema/inputs/emailNotification.input.ts
+++ src/schema/inputs/emailNotification.input.ts
@@ -64,6 +64,7 @@ export const DatasetInputType = new GraphQLInputObjectType({
     sendAsAttachment: { type: GraphQLBoolean },
     individualEmail: { type: GraphQLBoolean },
     individualEmailFields: { type: new GraphQLList(GraphQLJSON) },
+    csFilter: { type: GraphQLJSON },
     pageSize: { type: GraphQLInt },
     navigateToPage: { type: GraphQLBoolean, defaultValue: false },
     navigateSettings: { type: GraphQLJSON },
--- src/schema/types/emailNotification.type.ts
+++ src/schema/types/emailNotification.type.ts
@@ -43,6 +43,7 @@ export const DatasetType = new GraphQLObjectType({
     sendAsAttachment: { type: GraphQLBoolean },
     individualEmail: { type: GraphQLBoolean },
     individualEmailFields: { type: new GraphQLList(GraphQLJSON) },
+    csFilter: { type: GraphQLJSON },
     pageSize: { type: GraphQLInt },
     navigateToPage: { type: GraphQLBoolean, defaultValue: false },
     navigateSettings: { type: GraphQLJSON },
