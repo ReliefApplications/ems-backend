924d937c8230d494f87c3cd40424a74ed2b0eb1c
Morgan Gowans <morgan.g@adappt.ai>
2026-07-23 15:28:56 +0100

    Refactor: reuse common service auth vars

--- config/default.js
+++ config/default.js
@@ -40,12 +40,6 @@ module.exports = {
     serverless: {
       url: '',
       key: '',
-      auth: {
-        accessTokenUrl: '',
-        clientId: '',
-        clientSecret: '',
-        csScope: '',
-      },
     },
     // Redis channel on which the email function publishes its email events.
     eventsChannel: 'email-events',
